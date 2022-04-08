5586ff58ae648ee5afd0e7b18a7f575eb05fc6d0
William Rapprich <william.rapprich@t-systems.com>
2022-04-08 14:01:20 +0200

    Add GetAllFavouriteAppsForUser() method to AppsBusinessLogic

--- src/marketplace/CatenaX.NetworkServices.App.Service/BusinessLogic/IAppsBusinessLogic.cs
+++ src/marketplace/CatenaX.NetworkServices.App.Service/BusinessLogic/IAppsBusinessLogic.cs
@@ -5,5 +5,7 @@ namespace CatenaX.NetworkServices.App.Service.BusinessLogic
     public interface IAppsBusinessLogic
     {
         public Task<IEnumerable<AppViewModel>> GetAllActiveAppsAsync(string? languageShortName = null);
+
+        public Task<IEnumerable<Guid>> GetAllFavouriteAppsForUser(Guid userId);
     }
 }
