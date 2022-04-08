cee2b3dc9c8d6e20b1a0999e84dda98c06e347fa
William Rapprich <william.rapprich@t-systems.com>
2022-04-08 10:40:23 +0200

    Implement AppsController with first action without authentication

---
+++ src/marketplace/CatenaX.NetworkServices.App.Service/Controllers/AppsController.cs
@@ -0,0 +1,26 @@
+﻿using CatenaX.NetworkServices.App.Service.BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatenaX.NetworkServices.App.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppsController : ControllerBase
+    {
+        private readonly ILogger<AppsController> logger;
+        private readonly IAppsBusinessLogic appsBusinessLogic;
+
+        public AppsController(ILogger<AppsController> logger, IAppsBusinessLogic appsBusinessLogic)
+        {
+            this.logger = logger;
+            this.appsBusinessLogic = appsBusinessLogic;
+        }
+
+        [HttpGet]
+        [Route("active")]
+        public async Task<IActionResult> GetAllActiveApps([FromQuery] string? lang = null)
+        {
+            return Ok(await this.appsBusinessLogic.GetAllActiveAppsAsync(lang));
+        }
+    }
+}
