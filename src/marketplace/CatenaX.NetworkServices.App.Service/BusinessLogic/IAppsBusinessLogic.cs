06e3358c190f393698f810cb4b64045f7f086826
William Rapprich <william.rapprich@t-systems.com>
2022-04-08 10:39:20 +0200

    Add AppsBusinessLogic to AppService

---
+++ src/marketplace/CatenaX.NetworkServices.App.Service/BusinessLogic/IAppsBusinessLogic.cs
@@ -0,0 +1,9 @@
+﻿using CatenaX.NetworkServices.App.Service.ViewModels;
+
+namespace CatenaX.NetworkServices.App.Service.BusinessLogic
+{
+    public interface IAppsBusinessLogic
+    {
+        public Task<IEnumerable<AppViewModel>> GetAllActiveAppsAsync(string? languageShortName = null);
+    }
+}
