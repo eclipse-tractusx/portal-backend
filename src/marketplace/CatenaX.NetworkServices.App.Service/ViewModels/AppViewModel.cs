0ac5216bc9adb41b0a7e1620ff9f1c8cc82d0634
William Rapprich <william.rapprich@t-systems.com>
2022-04-08 10:36:47 +0200

    Add AppViewModel to AppService

---
+++ src/marketplace/CatenaX.NetworkServices.App.Service/ViewModels/AppViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CatenaX.NetworkServices.App.Service.ViewModels
+{
+    public class AppViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = default!;
+
+        public string ShortDescription { get; set; } = default!;
+
+        public string Provider { get; set; } = default!;
+
+        public ICollection<string> UseCases { get; set; } = new HashSet<string>();
+
+        public string Price { get; set; } = default!;
+
+        public string LeadPictureUri { get; set; } = default!;
+    }
+}
