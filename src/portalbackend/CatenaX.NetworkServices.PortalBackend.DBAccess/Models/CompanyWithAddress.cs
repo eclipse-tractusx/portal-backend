0cd0f449aa396a7e1ac446d4195f6a1b93c555b5
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-04-21 14:33:44 +0200

    CPLP-751: adjust PortalBackend.DBAccess to changed datamodel

--- src/portalbackend/CatenaX.NetworkServices.PortalBackend.DBAccess/Models/CompanyWithAddress.cs
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.DBAccess/Models/CompanyWithAddress.cs
@@ -8,13 +8,13 @@ namespace CatenaX.NetworkServices.PortalBackend.DBAccess.Models
         public string? Bpn { get; set; }
         public string? Name { get; set; }
         public string? Shortname { get; set; }
-        public string City { get; set; }
+        public string? City { get; set; }
         public string? Region { get; set; }
         public string? Streetadditional { get; set; }
-        public string Streetname { get; set; }
+        public string? Streetname { get; set; }
         public string? Streetnumber { get; set; }
-        public decimal Zipcode { get; set; }
-        public string CountryAlpha2Code { get; set; }
+        public decimal? Zipcode { get; set; }
+        public string? CountryAlpha2Code { get; set; }
         public string? CountryDe { get; set; }
     }
 }
