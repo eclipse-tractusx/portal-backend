c159610726d0710128842e6cf0e00757bf9b3c2f
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-04-21 21:33:03 +0200

    CPLP-751 adjust provisioning to datamodel and nullable

--- src/provisioning/CatenaX.NetworkServices.Provisioning.Service/Program.cs
+++ src/provisioning/CatenaX.NetworkServices.Provisioning.Service/Program.cs
@@ -18,7 +18,7 @@ namespace CatenaX.NetworkServices.Provisioning.Service
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostContext, builder) =>
                 {
-                    if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT").Equals("Kubernetes"))
+                    if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")?.Equals("Kubernetes") ?? false)
                     {
                         var provider = new PhysicalFileProvider("/app/secrets");
                         builder.AddJsonFile(provider, "appsettings.json", optional: false, reloadOnChange: false);
