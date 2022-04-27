98186a7f075df0e7c72d03db6a21e8f3d80ede74
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-04-27 10:56:17 +0200

    CPLP-730 cleanup redundant code

--- src/useradministration/CatenaX.NetworkServices.UserAdministration.Service/BusinessLogic/CompanyAdministrationBusinessLogic.cs
+++ src/useradministration/CatenaX.NetworkServices.UserAdministration.Service/BusinessLogic/CompanyAdministrationBusinessLogic.cs
@@ -26,19 +26,5 @@ namespace CatenaX.NetworkServices.UserAdministration.Service.BusinessLogic
             }
             return _portalDBAccess.GetCompanyWithAdressUntrackedAsync(applicationId.Value);
         }
-        
-        public Task SetCompanyWithAddressAsync(Guid? applicationId, CompanyWithAddress? companyWithAddress)
-        {
-            if (!applicationId.HasValue)
-            {
-                throw new ArgumentNullException("applicationId must not be null");
-            }
-            if (companyWithAddress == null)
-            {
-                throw new ArgumentNullException("companyWithAddress must not be null");
-            }
-            //FIXMX: add update of company status within same transpaction
-            return _portalDBAccess.SetCompanyWithAdressAsync(applicationId.Value, companyWithAddress);
-        }
     }
 }
