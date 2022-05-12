d5764d073106499955e1a527d1bb08472e8d9fc6
Akshay Chaudhari <akshay-dilip.chaudhari@partner.bmw.in>
2022-05-12 12:21:48 +0530

    cplp-628 - add document types

--- src/portalbackend/CatenaX.NetworkServices.PortalBackend.PortalEntities/Enums/DocumentTypeId.cs
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.PortalEntities/Enums/DocumentTypeId.cs
@@ -3,6 +3,9 @@ namespace CatenaX.NetworkServices.PortalBackend.PortalEntities.Enums
 {
     public enum DocumentTypeId : int
     {
-        DUMMY = 1
+        CXFrameContract = 1,
+        CommercialRegisterExtract = 2,
+        AppContract = 3,
+        DataContract = 4
     }
 }
