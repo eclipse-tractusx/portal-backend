2149a644f389bd74713685be0633e53a2161aa19
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-05-17 15:43:00 +0200

    CPLP-773 fix nullable warnings and package version conflicts

--- src/provisioning/CatenaX.NetworkServices.Provisioning.DBAccess/IProvisioningDBAccess.cs
+++ src/provisioning/CatenaX.NetworkServices.Provisioning.DBAccess/IProvisioningDBAccess.cs
@@ -1,16 +1,12 @@
-using System.Threading.Tasks;
 using CatenaX.NetworkServices.Provisioning.ProvisioningEntities;
-using System;
 
-namespace CatenaX.NetworkServices.Provisioning.DBAccess
+namespace CatenaX.NetworkServices.Provisioning.DBAccess;
 
+public interface IProvisioningDBAccess
 {
-    public interface IProvisioningDBAccess
-    {
-        Task<int> GetNextClientSequenceAsync();
-        Task<int> GetNextIdentityProviderSequenceAsync();
-        UserPasswordReset CreateUserPasswordResetInfo(string userEntityId, DateTimeOffset passwordModifiedAt, int resetCount);
-        Task<UserPasswordReset> GetUserPasswordResetInfo(string userEntityId);
-        Task<int> SaveAsync();
-    }
+    Task<int> GetNextClientSequenceAsync();
+    Task<int> GetNextIdentityProviderSequenceAsync();
+    UserPasswordReset CreateUserPasswordResetInfo(string userEntityId, DateTimeOffset passwordModifiedAt, int resetCount);
+    Task<UserPasswordReset?> GetUserPasswordResetInfo(string userEntityId);
+    Task<int> SaveAsync();
 }
