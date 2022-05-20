0880211b35bfe25c2575d02d6d42f5596de1eaf8
William Rapprich <william.rapprich@t-systems.com>
2022-05-20 12:45:22 +0200

    CPLP-786 Add Connector and related entities to PortalDbContext

---
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.PortalEntities/Entities/Connector.cs
@@ -0,0 +1,40 @@
+﻿using CatenaX.NetworkServices.PortalBackend.PortalEntities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CatenaX.NetworkServices.PortalBackend.PortalEntities.Entities;
+
+public class Connector
+{
+    public Connector(string name, string locationId, string connectorUrl)
+    {
+        Name = name;
+        LocationId = locationId;
+        ConnectorUrl = connectorUrl;
+    }
+
+    public Guid Id { get; set; }
+
+    [MaxLength(255)]
+    public string Name { get; set; }
+
+    [MaxLength(255)]
+    public string ConnectorUrl { get; set; }
+
+    public ConnectorTypeId TypeId { get; set; }
+
+    public ConnectorStatusId StatusId { get; set; }
+
+    public Guid ProviderId { get; set; }
+
+    public Guid? HostId { get; set; }
+
+    [StringLength(2, MinimumLength = 2)]
+    public string LocationId { get; set; }
+
+    // Navigation properties
+    public ConnectorType? Type { get; set; }
+    public ConnectorStatus? Status { get; set; }
+    public Company? Provider { get; set; }
+    public Company? Host { get; set; }
+    public Country? Location { get; set; }
+}
