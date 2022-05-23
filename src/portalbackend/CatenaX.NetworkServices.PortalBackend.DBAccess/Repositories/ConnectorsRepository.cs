e7db7bd15d6675d804a1dcdf48d31d1ec90d916f
William Rapprich <william.rapprich@t-systems.com>
2022-05-23 18:02:51 +0200

    CPLP-786 Add ConnectorsRepository to PortalBackend.DBAccess

---
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.DBAccess/Repositories/ConnectorsRepository.cs
@@ -0,0 +1,28 @@
+﻿using CatenaX.NetworkServices.PortalBackend.PortalEntities;
+using CatenaX.NetworkServices.PortalBackend.PortalEntities.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatenaX.NetworkServices.PortalBackend.DBAccess.Repositories;
+
+/// Implementation of <see cref="IConnectorsRepository"/> accessing database with EF Core.
+public class ConnectorsRepository : IConnectorsRepository
+{
+    private readonly PortalDbContext _context;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="portalDbContext">PortalDb context.</param>
+    public ConnectorsRepository(PortalDbContext portalDbContext)
+    {
+        this._context = portalDbContext;
+    }
+
+    /// <inheritdoc/>
+    public IQueryable<Connector> GetAllCompanyConnectorsForIamUser(string iamUserId)
+    {
+        return _context.IamUsers.AsNoTracking()
+            .Where(u => u.UserEntityId == iamUserId)
+            .SelectMany(u => u.CompanyUser!.Company!.ProvidedConnectors);
+    }
+}
---
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.DBAccess/Repositories/IConnectorsRepository.cs
@@ -0,0 +1,16 @@
+﻿using CatenaX.NetworkServices.PortalBackend.PortalEntities.Entities;
+
+namespace CatenaX.NetworkServices.PortalBackend.DBAccess.Repositories;
+
+/// <summary>
+/// Repository for accessing connectors on persistence layer.
+/// </summary>
+public interface IConnectorsRepository
+{
+    /// <summary>
+    /// Get all connectors of a user's company by iam user ID.
+    /// </summary>
+    /// <param name="iamUserId">ID of the iam user used to determine company's connectors for.</param>
+    /// <returns>Queryable of connectors that allows transformation.</returns>
+    public IQueryable<Connector> GetAllCompanyConnectorsForIamUser(string iamUserId);
+}
