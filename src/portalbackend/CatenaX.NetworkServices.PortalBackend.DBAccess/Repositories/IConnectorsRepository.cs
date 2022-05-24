982295782048f58af96658bbb49269f72c8f2372
William Rapprich <william.rapprich@t-systems.com>
2022-05-24 17:24:37 +0200

    CPLP-786 Add connector creation logic to ConnectorsRepository

--- src/portalbackend/CatenaX.NetworkServices.PortalBackend.DBAccess/Repositories/IConnectorsRepository.cs
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.DBAccess/Repositories/IConnectorsRepository.cs
@@ -13,4 +13,11 @@ public interface IConnectorsRepository
     /// <param name="iamUserId">ID of the iam user used to determine company's connectors for.</param>
     /// <returns>Queryable of connectors that allows transformation.</returns>
     public IQueryable<Connector> GetAllCompanyConnectorsForIamUser(string iamUserId);
+
+    /// <summary>
+    /// Creates a given connector in persistence layer. 
+    /// </summary>
+    /// <param name="connector">Connector to create.</param>
+    /// <returns>Created and persisted connector.</returns>
+    public Task<Connector> CreateConnectorAsync(Connector connector);
 }
