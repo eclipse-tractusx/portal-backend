01a7f61fb9999e75a9580c8fb357efefda748843
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-05-25 17:50:38 +0200

    CPLP-459 simplify assignment of user_roles

--- src/administration/CatenaX.NetworkServices.Administration.Service/BusinessLogic/UserBusinessLogic.cs
+++ src/administration/CatenaX.NetworkServices.Administration.Service/BusinessLogic/UserBusinessLogic.cs
@@ -356,38 +356,35 @@ namespace CatenaX.NetworkServices.Administration.Service.BusinessLogic
         public async Task<bool> ApprovePartnerRequest(Guid applicationId)
         {
             var companyApplication = await _portalDBAccess.GetCompanyAndApplicationForSubmittedApplication(applicationId).ConfigureAwait(false);
-            if (companyApplication != null)
+            if (companyApplication == null)
             {
-                bool success = false;
-                var clientId = _settings.Portal.KeyCloakClientID;
-                var companyRoleIds = await _portalDBAccess.GetUserRoleWithIdsUntrackedAsync(
-                     clientId,
-                     _settings.ApplicationApprovalInitialRoles.First().Value
-               )
-                .ToDictionaryAsync(
-                companyRoleWithId => companyRoleWithId.CompanyUserRoleText,
-                companyRoleWithId => companyRoleWithId.CompanyUserRoleId
-                )
-                .ConfigureAwait(false);
-
-                await foreach (var item in _portalDBAccess.GetInvitedUsersByApplicationIdUntrackedAsync(applicationId).ConfigureAwait(false))
-                {
-                    await _provisioningManager.AssignClientRolesToCentralUserAsync(item.UserEntityId, _settings.ApplicationApprovalInitialRoles).ConfigureAwait(false);
-                    await _provisioningManager.AddBpnAttributetoUserAsync(item.UserEntityId, Enumerable.Repeat(companyApplication.Company!.Bpn, 1));
-                    _portalDBAccess.CreateCompanyUserAssignedRole(item.CompanyUserId, companyRoleIds[_settings.ApplicationApprovalInitialRoles.First().Value.FirstOrDefault()]);
-                }
-                companyApplication.Company!.CompanyStatusId = CompanyStatusId.ACTIVE;
-                companyApplication.ApplicationStatusId = CompanyApplicationStatusId.SUBMITTED;
-                companyApplication.DateLastChanged = DateTimeOffset.UtcNow;
-                await _portalDBAccess.SaveAsync().ConfigureAwait(false);
-
-                await _custodianService.CreateWallet(companyApplication.Company!.Bpn, companyApplication.Company!.Name).ConfigureAwait(false);
-                await PostRegistrationWelcomeEmailAsync(applicationId).ConfigureAwait(false);
-
-                success = true;
-                return success;
+                throw new ArgumentException($"CompanyApplication {applicationId} is not in status SUBMITTED","applicationId");
             }
-            throw new ArgumentException($"CompanyApplication {applicationId} is in wrong status");
+            if (companyApplication.Company!.Bpn == null)
+            {
+                throw new ArgumentException($"BusinessPartnerNumber (bpn) for CompanyApplications {applicationId} company {companyApplication.CompanyId} is null","bpn");
+            }
+            var userRoleIds = await _portalDBAccess.GetUserRoleIdsUntrackedAsync(_settings.ApplicationApprovalInitialRoles).ToListAsync().ConfigureAwait(false);
+
+            await foreach (var item in _portalDBAccess.GetInvitedUsersByApplicationIdUntrackedAsync(applicationId).ConfigureAwait(false))
+            {
+                await _provisioningManager.AssignClientRolesToCentralUserAsync(item.UserEntityId, _settings.ApplicationApprovalInitialRoles).ConfigureAwait(false);
+                await _provisioningManager.AddBpnAttributetoUserAsync(item.UserEntityId, Enumerable.Repeat(companyApplication.Company.Bpn, 1));
+                foreach(var userRoleId in userRoleIds)
+                {
+                    _portalDBAccess.CreateCompanyUserAssignedRole(item.CompanyUserId, userRoleId);
+                }
+            }
+            companyApplication.Company!.CompanyStatusId = CompanyStatusId.ACTIVE;
+            companyApplication.ApplicationStatusId = CompanyApplicationStatusId.SUBMITTED;
+            companyApplication.DateLastChanged = DateTimeOffset.UtcNow;
+
+            await _portalDBAccess.SaveAsync().ConfigureAwait(false);
+
+            await _custodianService.CreateWallet(companyApplication.Company.Bpn, companyApplication.Company.Name).ConfigureAwait(false);
+            await PostRegistrationWelcomeEmailAsync(applicationId).ConfigureAwait(false);
+
+            return true;
         }
     }
 }
