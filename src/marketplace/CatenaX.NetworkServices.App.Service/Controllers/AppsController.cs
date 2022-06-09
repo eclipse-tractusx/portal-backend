373bf3c80d4c6914715d65e018587a9fd721a02c
Philip Schneider <P.Schneider@external.t-systems.com>
2022-06-09 12:44:21 +0200

    CPLP-1056 change unsubscribe response to 204

--- src/marketplace/CatenaX.NetworkServices.App.Service/Controllers/AppsController.cs
+++ src/marketplace/CatenaX.NetworkServices.App.Service/Controllers/AppsController.cs
@@ -151,16 +151,16 @@ public class AppsController : ControllerBase
     /// </summary>
     /// <param name="appId" example="D3B1ECA2-6148-4008-9E6C-C1C2AEA5C645">ID of the app to unsubscribe from.</param>
     /// <remarks>Example: PUT: /api/apps/D3B1ECA2-6148-4008-9E6C-C1C2AEA5C645/unsubscribe</remarks>
-    /// <response code="202">The app was successfully unsubscribed from.</response>
+    /// <response code="204">The app was successfully unsubscribed from.</response>
     /// <response code="400">If sub claim is empty/invalid or user does not exist.</response>
     [HttpPut]
     [Route("{appId}/unsubscribe")]
     [Authorize(Roles = "unsubscribe_apps")]
-    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> UnsubscribeCompanyAppSubscriptionAsync([FromRoute] Guid appId)
     {
         await this.WithIamUserId(userId => this.appsBusinessLogic.UnsubscribeCompanyAppSubscriptionAsync(appId, userId));
-        return this.Accepted();
+        return this.NoContent();
     }
 }
