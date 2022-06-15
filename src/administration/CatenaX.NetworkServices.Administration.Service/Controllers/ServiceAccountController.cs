aa19ea9b9f5f81923a0bc5f08390100280c94a60
Philip Schneider <info@philschneider.de>
2022-06-15 13:28:25 +0200

    CPLP-1087 add swagger documentation

--- src/administration/CatenaX.NetworkServices.Administration.Service/Controllers/ServiceAccountController.cs
+++ src/administration/CatenaX.NetworkServices.Administration.Service/Controllers/ServiceAccountController.cs
@@ -1,5 +1,6 @@
 using CatenaX.NetworkServices.Administration.Service.BusinessLogic;
 using CatenaX.NetworkServices.Administration.Service.Models;
+using CatenaX.NetworkServices.Framework.ErrorHandling;
 using CatenaX.NetworkServices.Framework.Models;
 using CatenaX.NetworkServices.Keycloak.Authentication;
 using CatenaX.NetworkServices.PortalBackend.DBAccess.Models;
@@ -14,6 +15,11 @@ namespace CatenaX.NetworkServices.Administration.Service.Controllers;
 public class ServiceAccountController : ControllerBase
 {
     private readonly IServiceAccountBusinessLogic _logic;
+    
+    /// <summary>
+    /// Creates a new instance of <see cref="ServiceAccountController"/> 
+    /// </summary>
+    /// <param name="logic">The Service Account Buisness Logic</param>
     public ServiceAccountController(IServiceAccountBusinessLogic logic)
     {
         _logic = logic;
@@ -24,12 +30,21 @@ public class ServiceAccountController : ControllerBase
     /// </summary>
     /// <param name="serviceAccountCreationInfo"></param>
     /// <returns></returns>
-    /// <response code="201">The service account was created..</response>
+    /// Example: POST: api/administration/serviceaccount/owncompany/serviceaccounts
+    /// <response code="201">The service account was created.</response>
+    /// <response code="400">
+    /// Problem could be one of the following: <br />
+    /// - other authenticationType values than SECRET are not supported yet <br />
+    /// - name is empty <br />
+    /// - user is not associated with any company <br />
+    /// - a user role id is not a valid user role id
+    /// </response>
     /// <response code="401">User is unauthorized.</response>
     [HttpPost]
     [Authorize(Roles = "add_tech_user_management")]
     [Route("owncompany/serviceaccounts")]
     [ProducesResponseType(typeof(ServiceAccountDetails), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<ActionResult<ServiceAccountDetails>> ExecuteCompanyUserCreation([FromBody] ServiceAccountCreationInfo serviceAccountCreationInfo)
     {
@@ -40,15 +55,20 @@ public class ServiceAccountController : ControllerBase
     /// <summary>
     /// Deletes the service account with the given id
     /// </summary>
-    /// <param name="serviceAccountId">Id of the service account that should be deleted.</param>
+    /// <param name="serviceAccountId" example="7e85a0b8-0001-ab67-10d1-0ef508201000">Id of the service account that should be deleted.</param>
     /// <returns></returns>
+    /// Example: DELETE: api/administration/owncompany/serviceaccounts/7e85a0b8-0001-ab67-10d1-0ef508201000
     /// <response code="200">Successful if the service account was deleted.</response>
     /// <response code="401">User is unauthorized.</response>
+    /// <response code="404">ServiceAccount not found in company of the user.</response>
+    /// <response code="500">Internal server error occured, e.g. a database error.</response>
     [HttpDelete]
     [Authorize(Roles = "delete_tech_user_management")]
     [Route("owncompany/serviceaccounts/{serviceAccountId}")]
     [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
     [ProducesResponseType( StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public Task<int> DeleteServiceAccount([FromRoute] Guid serviceAccountId) =>
         this.WithIamUserId(adminId => _logic.DeleteOwnCompanyServiceAccountAsync(serviceAccountId, adminId));
 
@@ -57,13 +77,18 @@ public class ServiceAccountController : ControllerBase
     /// </summary>
     /// <param name="serviceAccountId">Id to get the service account details for.</param>
     /// <returns>Returns a list of service account details.</returns>
+    /// Example: GET: api/administration/serviceaccount/owncompany/serviceaccounts/7e85a0b8-0001-ab67-10d1-0ef508201000
     /// <response code="200">Returns a list of service account details.</response>
     /// <response code="401">User is unauthorized.</response>
+    /// <response code="404">ServiceAccount not found in company of the user.</response>
+    /// <response code="500">Internal server error occured, e.g. a database error.</response>
     [HttpGet]
     [Authorize(Roles = "view_tech_user_management")]
     [Route("owncompany/serviceaccounts/{serviceAccountId}", Name="GetServiceAccountDetails")]
     [ProducesResponseType(typeof(ServiceAccountDetails), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public Task<ServiceAccountDetails> GetServiceAccountDetails([FromRoute] Guid serviceAccountId) =>
         this.WithIamUserId(adminId => _logic.GetOwnCompanyServiceAccountDetailsAsync(serviceAccountId, adminId));
 
@@ -73,28 +98,46 @@ public class ServiceAccountController : ControllerBase
     /// <param name="serviceAccountId">Id of the service account details that should be updated.</param>
     /// <param name="serviceAccountDetails">The new values for the details.</param>
     /// <returns>Returns the updated service account details.</returns>
+    /// Example: PUT: api/administration/serviceaccount/owncompany/serviceaccounts/7e85a0b8-0001-ab67-10d1-0ef508201000
     /// <response code="200">Returns the updated service account details.</response>
+    /// <response code="400">
+    /// Problem could be one of the following: <br />
+    /// - other authenticationType values than SECRET are not supported yet <br />
+    /// - serviceAccountId from path does not match the one in body <br />
+    /// - serviceAccount is already INACTIVE <br />
+    /// </response>
     /// <response code="401">User is unauthorized.</response>
+    /// <response code="404">ServiceAccount not found in company of the user.</response>
+    /// <response code="500">Internal server error occured, e.g. a database error.</response>
     [HttpPut]
     [Authorize(Roles = "add_tech_user_management")] // TODO check whether we also want an edit role
     [Route("owncompany/serviceaccounts/{serviceAccountId}")]
     [ProducesResponseType(typeof(ServiceAccountDetails), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public Task<ServiceAccountDetails> PutServiceAccountDetails([FromRoute] Guid serviceAccountId, [FromBody] ServiceAccountEditableDetails serviceAccountDetails) =>
         this.WithIamUserId(adminId => _logic.UpdateOwnCompanyServiceAccountDetailsAsync(serviceAccountId, serviceAccountDetails, adminId));
 
     /// <summary>
     /// Resets the service account credentials for the given service account Id.
+    /// 
     /// </summary>
     /// <param name="serviceAccountId">Id of the service account.</param>
     /// <returns>Returns the service account details.</returns>
+    /// Example: PUT: api/administration/serviceaccount/owncompany/serviceaccounts/7e85a0b8-0001-ab67-10d1-0ef508201000/resetCredentials
     /// <response code="200">Returns the service account details.</response>
     /// <response code="401">User is unauthorized.</response>
+    /// <response code="404">ServiceAccount not found in company of the user.</response>
+    /// <response code="500">Internal server error occured, e.g. a database error.</response>
     [HttpPost]
     [Authorize(Roles = "add_tech_user_management")]
     [Route("owncompany/serviceaccounts/{serviceAccountId}/resetCredentials")]
     [ProducesResponseType(typeof(ServiceAccountDetails), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public Task<ServiceAccountDetails> ResetServiceAccountCredentials([FromRoute] Guid serviceAccountId) =>
         this.WithIamUserId(adminId => _logic.ResetOwnCompanyServiceAccountSecretAsync(serviceAccountId, adminId));
 
@@ -104,13 +147,18 @@ public class ServiceAccountController : ControllerBase
     /// <param name="page">the page of service account data</param>
     /// <param name="size">number of service account data</param>
     /// <returns>Returns the specific number of service account data for the given page.</returns>
+    /// Example: GET: api/administration/serviceaccount/owncompany/serviceaccounts
     /// <response code="200">Returns the specific number of service account data for the given page.</response>
+    /// <response code="400">User is not associated with any company.</response>
     /// <response code="401">User is unauthorized.</response>
+    /// <response code="500">Internal server error occured, e.g. a database error.</response>
     [HttpGet]
     [Authorize(Roles = "view_tech_user_management")]
     [Route("owncompany/serviceaccounts")]
     [ProducesResponseType(typeof(Pagination.Response<CompanyServiceAccountData>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
     [ProducesResponseType( StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public Task<Pagination.Response<CompanyServiceAccountData>> GetServiceAccountsData([FromQuery] int page, [FromQuery] int size) =>
         this.WithIamUserId(adminId => _logic.GetOwnCompanyServiceAccountsDataAsync(page, size, adminId));
 }
