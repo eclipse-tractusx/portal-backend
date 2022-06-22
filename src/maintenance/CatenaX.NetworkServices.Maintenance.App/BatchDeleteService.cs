e343e85f17e6d85a55f85ce2debaf43a2f40009b
Philip Schneider <info@philschneider.de>
2022-06-22 13:03:28 +0200

    CPLP-1104 add maintenance for batch delete of documents

---
+++ src/maintenance/CatenaX.NetworkServices.Maintenance.App/BatchDeleteService.cs
@@ -0,0 +1,30 @@
+﻿using CatenaX.NetworkServices.PortalBackend.PortalEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatenaX.NetworkServices.Maintenance.App
+{
+    /// <summary>
+    /// Service to delete the pending and inactive documents from the database
+    /// </summary>
+    public static class BatchDeleteService
+    {
+        /// <summary>
+        /// Deletes the inactive and pending documents that are older than the specified days
+        /// </summary>
+        /// <param name="dbContext"></param>
+        /// <param name="days"></param>
+        /// <exception cref="Exception"></exception>
+        public static void BatchDeleteDocuments(PortalDbContext dbContext, int days)
+        {
+            try
+            {
+                dbContext.Database.ExecuteSqlRaw(
+                    $"delete from portal.documents where date_created < now() - interval '{days} days' and (document_status_id = 1 or document_status_id = 3)");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ src/maintenance/CatenaX.NetworkServices.Maintenance.App/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+using CatenaX.NetworkServices.Maintenance.App;
+using CatenaX.NetworkServices.PortalBackend.PortalEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+var environmentName = Environment.GetEnvironmentVariable("DOTNETCORE_ENVIRONMENT");
+// Build a config object, using env vars and JSON providers.
+var configBuilder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+    .AddEnvironmentVariables()
+    .AddUserSecrets(Assembly.GetExecutingAssembly());
+
+if (environmentName == "Kubernetes")
+{
+    var provider = new PhysicalFileProvider("/app/secrets");
+    configBuilder.AddJsonFile(provider, "appsettings.json", optional: false, reloadOnChange: true);
+}
+
+var configuration = configBuilder.Build();
+
+var host = Host.CreateDefaultBuilder(args)
+    .Build();
+var services = new ServiceCollection();
+services.AddDbContext<PortalDbContext>(o => o.UseNpgsql(configuration.GetConnectionString("PortalDb")));
+ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+BatchDeleteService.BatchDeleteDocuments(serviceProvider.GetRequiredService<PortalDbContext>(), configuration.GetValue<int>("DeleteIntervalInDays"));
+
+host.RunAsync();
