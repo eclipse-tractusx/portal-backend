1e338fab46dec3bd76aa064e397e532c7e8c28c7
Philip Schneider <info@philschneider.de>
2022-06-23 16:52:20 +0200

    CPLP-1104 Add maintenance worker to delete old documents

--- src/maintenance/CatenaX.NetworkServices.Maintenance.App/BatchDeleteService.cs
+++ src/maintenance/CatenaX.NetworkServices.Maintenance.App/BatchDeleteService.cs
@@ -1,30 +1,52 @@
 ﻿using CatenaX.NetworkServices.PortalBackend.PortalEntities;
 using Microsoft.EntityFrameworkCore;
 
-namespace CatenaX.NetworkServices.Maintenance.App
+namespace CatenaX.NetworkServices.Maintenance.App;
+
+/// <summary>
+/// Service to delete the pending and inactive documents as well as the depending consents from the database
+/// </summary>
+public class BatchDeleteService : BackgroundService
 {
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly ILogger<BatchDeleteService> _logger;
+    private readonly int _days;
+
     /// <summary>
-    /// Service to delete the pending and inactive documents from the database
+    /// Creates a new instance of <see cref="BatchDeleteService"/>
     /// </summary>
-    public static class BatchDeleteService
+    /// <param name="serviceScopeFactory">access to the services</param>
+    /// <param name="logger">the logger</param>
+    /// <param name="config">the apps configuration</param>
+    public BatchDeleteService(IServiceScopeFactory serviceScopeFactory, ILogger<BatchDeleteService> logger, IConfiguration config)
     {
-        /// <summary>
-        /// Deletes the inactive and pending documents that are older than the specified days
-        /// </summary>
-        /// <param name="dbContext"></param>
-        /// <param name="days"></param>
-        /// <exception cref="Exception"></exception>
-        public static void BatchDeleteDocuments(PortalDbContext dbContext, int days)
+        _serviceScopeFactory = serviceScopeFactory;
+        _logger = logger;
+        _days = config.GetValue<int>("DeleteIntervalInDays");
+    }
+
+    /// <inheritdoc />
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<PortalDbContext>();
+            
+        using var transaction = dbContext.Database.BeginTransaction();
+        try
         {
-            try
-            {
-                dbContext.Database.ExecuteSqlRaw(
-                    $"delete from portal.documents where date_created < now() - interval '{days} days' and (document_status_id = 1 or document_status_id = 3)");
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            _logger.LogInformation("Cleaning up consents...");
+            dbContext.Database.ExecuteSqlRaw($"DELETE FROM portal.consents WHERE document_id IN (SELECT id FROM portal.documents where date_created < now() - interval '{_days} days' and (document_status_id = 1 or document_status_id = 3))");
+            _logger.LogInformation("Cleaning up documents...");
+            dbContext.Database.ExecuteSqlRaw($"DELETE FROM portal.documents where date_created < now() - interval '{_days} days' and (document_status_id = 1 or document_status_id = 3)");
+            transaction.Commit();
+            _logger.LogInformation($"Documents older than {_days} days and depending consents successfully cleaned up.");
         }
+        catch (Exception ex)
+        {
+            transaction.Rollback();
+            _logger.LogError($"Database clean up failed with error: {ex.Message}");
+        }
+
+        return Task.CompletedTask;
     }
 }
