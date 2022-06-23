e9ebe100d17c3cbd766f07dab5bc90604ae77af5
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-06-24 00:44:35 +0200

    CPLP-1039 refactoring for repository-pattern

--- src/registration/CatenaX.NetworkServices.Registration.Service/Controllers/RegistrationController.cs
+++ src/registration/CatenaX.NetworkServices.Registration.Service/Controllers/RegistrationController.cs
@@ -102,7 +102,7 @@ namespace CatenaX.NetworkServices.Registration.Service.Controllers
         [HttpGet]
         [Authorize(Roles = "view_registration")]
         [Route("applications")]
-        public IAsyncEnumerable<CompanyApplication> GetApplicationsWithStatusAsync() =>
+        public IAsyncEnumerable<CompanyApplicationData> GetApplicationsWithStatusAsync() =>
             this.WithIamUserId(user => _registrationBusinessLogic.GetAllApplicationsForUserWithStatus(user));
 
         [HttpPut]
@@ -127,7 +127,8 @@ namespace CatenaX.NetworkServices.Registration.Service.Controllers
         [Authorize(Roles = "add_company_data")]
         [Route("application/{applicationId}/companyDetailsWithAddress")]
         public Task SetCompanyWithAddressAsync([FromRoute] Guid applicationId, [FromBody] CompanyWithAddress companyWithAddress) =>
-            _registrationBusinessLogic.SetCompanyWithAddressAsync(applicationId, companyWithAddress);
+            this.WithIamUserId(iamUserId =>
+                _registrationBusinessLogic.SetCompanyWithAddressAsync(applicationId, companyWithAddress, iamUserId));
 
         [HttpPost]
         [Authorize(Roles = "invite_user")]
@@ -162,7 +163,6 @@ namespace CatenaX.NetworkServices.Registration.Service.Controllers
         public Task<bool> SubmitRegistrationAsync([FromRoute] Guid applicationId) =>
             this.WithIamUserId(iamUserId =>
                 _registrationBusinessLogic.SubmitRegistrationAsync(applicationId, iamUserId));
-
 
         [HttpGet]
         [Authorize(Roles = "view_registration")]
