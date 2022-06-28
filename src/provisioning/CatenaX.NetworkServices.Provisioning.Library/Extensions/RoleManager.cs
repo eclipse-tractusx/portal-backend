0a0d52781a6914e7ccce79904ef8fc10eed0367f
qxz2mqe <md-majid.akhter@partner.bmw.in>
2022-06-28 20:22:29 +0530

    CPLP-1115 fixed multiple roles issue scenario2

--- src/provisioning/CatenaX.NetworkServices.Provisioning.Library/Extensions/RoleManager.cs
+++ src/provisioning/CatenaX.NetworkServices.Provisioning.Library/Extensions/RoleManager.cs
@@ -1,5 +1,6 @@
 using CatenaX.NetworkServices.Framework.ErrorHandling;
 using Keycloak.Net.Models.Roles;
+using System.Collections;
 
 namespace CatenaX.NetworkServices.Provisioning.Library
 {
@@ -17,26 +18,22 @@ namespace CatenaX.NetworkServices.Provisioning.Library
                     return (client.Id, Enumerable.Repeat<Role>(await _CentralIdp.GetRoleByNameAsync(_Settings.CentralRealm, client.Id, roleNames.Single()).ConfigureAwait(false), 1));
                 default:
                     var roles = (await _CentralIdp.GetRolesAsync(_Settings.CentralRealm, client.Id).ConfigureAwait(false)).Where(x => roleNames.Contains(x.Name));
-                    if (roles.Count() != count)
-                    {
-                        throw new NotFoundException($"invalid roles for client {clientName}: [{String.Join(",",roleNames.Except(roles.Select(role => role.Name)))}]");
-                    }
                     return (client.Id, roles);
             }
         }
 
-        public async Task AssignClientRolesToCentralUserAsync(string centralUserId, IDictionary<string, IEnumerable<string>> clientRoleNames)
+        public async Task<IEnumerable<string>> AssignClientRolesToCentralUserAsync(string centralUserId, IDictionary<string, IEnumerable<string>> clientRoleNames)
         {
-            if (!(await Task.WhenAll(clientRoleNames.Select(async x =>
+            var results = await Task.WhenAll(clientRoleNames.Select(async x =>
                 {
                     var (client, roleNames) = x;
                     var (clientId, roles) = await GetCentralClientIdRolesAsync(client, roleNames).ConfigureAwait(false);
-                    return await _CentralIdp.AddClientRoleMappingsToUserAsync(_Settings.CentralRealm, centralUserId, clientId, roles).ConfigureAwait(false);
+                    await _CentralIdp.AddClientRoleMappingsToUserAsync(_Settings.CentralRealm, centralUserId, clientId, roles).ConfigureAwait(false);
+                    return roles;
+                    
                 }
-            )).ConfigureAwait(false)).All(x => x))
-            {
-                throw new Exception($"failed to assign client-roles {clientRoleNames} to central user {centralUserId}");
-            }
+            )).ConfigureAwait(false);
+            return results.Select(role => role.FirstOrDefault().Name);
         }
     }
 }
