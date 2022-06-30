b8a3a4bb6fc721e12ea611b3b31f1b5755d04355
Philip Schneider <info@philschneider.de>
2022-06-30 15:16:03 +0200

    CPLP-1134 add notification

---
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.PortalEntities/Entities/NotificationStatus.cs
@@ -0,0 +1,43 @@
+﻿using CatenaX.NetworkServices.PortalBackend.PortalEntities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CatenaX.NetworkServices.PortalBackend.PortalEntities.Entities;
+
+/// <summary>
+/// Status of a notification
+/// </summary>
+public class NotificationStatus
+{
+    /// <summary>
+    /// Internal constructor, only for EF
+    /// </summary>
+    private NotificationStatus()
+    {
+        Label = null!;
+        Notifications = new HashSet<Notification>();
+    }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="NotificationStatus"/> and initializes the id and label 
+    /// </summary>
+    /// <param name="notificationStatusId">The NotificationStatusId</param>
+    public NotificationStatus(NotificatioStatusId notificationStatusId) : this()
+    {
+        Id = notificationStatusId;
+        Label = notificationStatusId.ToString();
+    }
+
+    /// <summary>
+    /// Id of the status
+    /// </summary>
+    public NotificatioStatusId Id { get; private set; }
+
+    /// <summary>
+    /// The status as string 
+    /// </summary>
+    [MaxLength(255)]
+    public string Label { get; private set; }
+
+    // Navigation properties
+    public virtual ICollection<Notification> Notifications { get; private set; }
+}
---
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.PortalEntities/Entities/NotificationType.cs
@@ -0,0 +1,43 @@
+﻿using CatenaX.NetworkServices.PortalBackend.PortalEntities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CatenaX.NetworkServices.PortalBackend.PortalEntities.Entities;
+
+/// <summary>
+/// Type of a notification
+/// </summary>
+public class NotificationType
+{
+    /// <summary>
+    /// Internal constructor, only for EF
+    /// </summary>
+    private NotificationType()
+    {
+        Label = null!;
+        Notifications = new HashSet<Notification>();
+    }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="NotificationType"/> and initializes the id and label 
+    /// </summary>
+    /// <param name="notificationTypeId">The NotificationTypesId</param>
+    public NotificationType(NotificationTypeId notificationTypeId) : this()
+    {
+        Id = notificationTypeId;
+        Label = notificationTypeId.ToString();
+    }
+
+    /// <summary>
+    /// Id of the type
+    /// </summary>
+    public NotificationTypeId Id { get; private set; }
+
+    /// <summary>
+    /// The type as string 
+    /// </summary>
+    [MaxLength(255)]
+    public string Label { get; private set; }
+
+    // Navigation properties
+    public virtual ICollection<Notification> Notifications { get; private set; }
+}
