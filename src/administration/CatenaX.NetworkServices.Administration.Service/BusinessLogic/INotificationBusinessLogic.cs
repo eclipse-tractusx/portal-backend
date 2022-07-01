8c2549341d923f8ff995d1c148c54eab317f4722
Philip Schneider <info@philschneider.de>
2022-07-01 15:42:28 +0200

    CPLP-1133 add create notification endpoint

---
+++ src/administration/CatenaX.NetworkServices.Administration.Service/BusinessLogic/INotificationBusinessLogic.cs
@@ -0,0 +1,16 @@
+﻿using CatenaX.NetworkServices.PortalBackend.DBAccess.Models;
+
+namespace CatenaX.NetworkServices.Administration.Service.BusinessLogic;
+
+/// <summary>
+/// Business logic to work with the notifications
+/// </summary>
+public interface INotificationBusinessLogic
+{
+    /// <summary>
+    /// Creates a new Notification with the given data
+    /// </summary>
+    /// <param name="creationData">The data for the creation of the notification.</param>
+    /// <param name="companyUserId">Id of the company user the notification is intended for.</param>
+    Task<NotificationDetailData> CreateNotification(NotificationCreationData creationData, Guid companyUserId);
+}
