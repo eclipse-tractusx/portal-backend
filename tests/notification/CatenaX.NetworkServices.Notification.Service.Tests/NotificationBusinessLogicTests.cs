2134d45916bb89cf30fb43232ff9af53df1f1d98
Philip Schneider <info@philschneider.de>
2022-07-04 15:42:32 +0200

    CPLP-1133 add success test case for notification

--- tests/notification/CatenaX.NetworkServices.Notification.Service.Tests/NotificationBusinessLogicTests.cs
+++ tests/notification/CatenaX.NetworkServices.Notification.Service.Tests/NotificationBusinessLogicTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoFixture;
 using AutoFixture.AutoFakeItEasy;
@@ -11,19 +13,22 @@ using CatenaX.NetworkServices.PortalBackend.PortalEntities.Enums;
 using FakeItEasy;
 using FluentAssertions;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace CatenaX.NetworkServices.Notification.Service.Tests
 {
     public class NotificationBusinessLogicTests
     {
+        private readonly ITestOutputHelper _testOutputHelper;
         private readonly IFixture _fixture;
         private readonly IPortalRepositories _portalRepositories;
         private readonly INotificationRepository _notificationRepository;
         private readonly IUserRepository _userRepository;
         private readonly CompanyUser _companyUser;
 
-        public NotificationBusinessLogicTests()
+        public NotificationBusinessLogicTests(ITestOutputHelper testOutputHelper)
         {
+            _testOutputHelper = testOutputHelper;
             _fixture = new Fixture().Customize(new AutoFakeItEasyCustomization { ConfigureMembers = true });
             var (companyUser, _) = CreateTestUserPair();
             _companyUser = companyUser;
@@ -34,14 +39,40 @@ namespace CatenaX.NetworkServices.Notification.Service.Tests
                 .ReturnsLazily(() => Task.FromResult(true));
             A.CallTo(() => _userRepository.IsUserWithIdExisting(A<Guid>.That.Not.Matches(x => x == companyUser.Id)))
                 .ReturnsLazily(() => Task.FromResult(false));
+            A.CallTo(() => _portalRepositories.GetInstance<IUserRepository>()).Returns(_userRepository);
+            A.CallTo(() => _portalRepositories.GetInstance<INotificationRepository>()).Returns(_notificationRepository);
+        }
+
+        [Fact]
+        public async Task CreateNotification_WithValidData_ReturnsCorrectDetails()
+        {
+
+            // Arrange
+            var notifications = new List<PortalBackend.PortalEntities.Entities.Notification>();
+            A.CallTo(() => _notificationRepository.Add(A<PortalBackend.PortalEntities.Entities.Notification>._))
+                .Invokes(action => notifications.Add(action.GetArgument<PortalBackend.PortalEntities.Entities.Notification>("notification")));
+            _fixture.Inject(_portalRepositories);
+            var sut = _fixture.Create<NotificationBusinessLogic>();
+            const string title = "That's a title";
+            const string message = "Here is a message for the reader";
+
+            // Act
+            var result = await sut.CreateNotification(new NotificationCreationData(DateTimeOffset.Now, title, message, NotificationTypeId.INFO, NotificationStatusId.UNREAD), _companyUser.Id);
+
+            // Assert
+            result.Should().NotBeNull();
+            notifications.Should().HaveCount(1);
+            var notification = notifications.Single();
+            notification.Should().NotBeNull();
+            notification.Title.Should().Be(title);
+            notification.Message.Should().Be(message);
         }
 
         [Fact]
         public async Task CreateNotification_WithInvalidNotificationType_ThrowsArgumentException()
         {
             // Arrange
-            A.CallTo(() => _portalRepositories.GetInstance<IUserRepository>()).Returns(_userRepository);
-            A.CallTo(() => _portalRepositories.GetInstance<INotificationRepository>()).Returns(_notificationRepository);
+            _fixture.Inject(_portalRepositories);
             var sut = _fixture.Create<NotificationBusinessLogic>();
 
             // Act
@@ -64,8 +95,7 @@ namespace CatenaX.NetworkServices.Notification.Service.Tests
         public async Task CreateNotification_WithInvalidNotificationStatus_ThrowsArgumentException()
         {
             // Arrange
-            A.CallTo(() => _portalRepositories.GetInstance<IUserRepository>()).Returns(_userRepository);
-            A.CallTo(() => _portalRepositories.GetInstance<INotificationRepository>()).Returns(_notificationRepository);
+            _fixture.Inject(_portalRepositories);
             var sut = _fixture.Create<NotificationBusinessLogic>();
 
             // Act
@@ -88,8 +118,6 @@ namespace CatenaX.NetworkServices.Notification.Service.Tests
         public async Task CreateNotification_WithNotExistingCompanyUser_ThrowsArgumentException()
         {
             // Arrange
-            A.CallTo(() => _portalRepositories.GetInstance<IUserRepository>()).Returns(_userRepository);
-            A.CallTo(() => _portalRepositories.GetInstance<INotificationRepository>()).Returns(_notificationRepository);
             _fixture.Inject(_portalRepositories);
             var sut = _fixture.Create<NotificationBusinessLogic>();
 
