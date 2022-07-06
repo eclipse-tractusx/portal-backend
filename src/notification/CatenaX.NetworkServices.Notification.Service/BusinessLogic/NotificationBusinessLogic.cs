c6e9f84106e70738406a53b20d006cde532fd89e
Philip Schneider <info@philschneider.de>
2022-07-06 11:44:16 +0200

    CPLP-1133 Add File Header

--- src/notification/CatenaX.NetworkServices.Notification.Service/BusinessLogic/NotificationBusinessLogic.cs
+++ src/notification/CatenaX.NetworkServices.Notification.Service/BusinessLogic/NotificationBusinessLogic.cs
@@ -1,4 +1,24 @@
-﻿using CatenaX.NetworkServices.PortalBackend.DBAccess;
+﻿// /********************************************************************************
+//  * Copyright (c) 2021,2022 BMW Group AG
+//  * Copyright (c) 2021,2022 Contributors to the CatenaX (ng) GitHub Organisation.
+//  *
+//  * See the NOTICE file(s) distributed with this work for additional
+//  * information regarding copyright ownership.
+//  *
+//  * This program and the accompanying materials are made available under the
+//  * terms of the Apache License, Version 2.0 which is available at
+//  * https://www.apache.org/licenses/LICENSE-2.0.
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//  * License for the specific language governing permissions and limitations
+//  * under the License.
+//  *
+//  * SPDX-License-Identifier: Apache-2.0
+//  ********************************************************************************/
+
+using CatenaX.NetworkServices.PortalBackend.DBAccess;
 using CatenaX.NetworkServices.PortalBackend.DBAccess.Models;
 using CatenaX.NetworkServices.PortalBackend.DBAccess.Repositories;
 using CatenaX.NetworkServices.PortalBackend.PortalEntities.Enums;
@@ -11,7 +31,7 @@ public class NotificationBusinessLogic : INotificationBusinessLogic
     private readonly IPortalRepositories _portalRepositories;
 
     /// <summary>
-    /// Creates a new instance of <see cref="NotificationBusinessLogic"/>
+    ///     Creates a new instance of <see cref="NotificationBusinessLogic" />
     /// </summary>
     /// <param name="portalRepositories">Access to the repository factory.</param>
     public NotificationBusinessLogic(IPortalRepositories portalRepositories)
@@ -20,41 +40,40 @@ public class NotificationBusinessLogic : INotificationBusinessLogic
     }
 
     /// <inheritdoc />
-    public async Task<NotificationDetailData> CreateNotification(NotificationCreationData creationData, Guid companyUserId)
+    public async Task<NotificationDetailData> CreateNotification(NotificationCreationData creationData,
+        Guid companyUserId)
     {
         if (!await _portalRepositories.GetInstance<IUserRepository>().IsUserWithIdExisting(companyUserId))
-        {
             throw new ArgumentException("User does not exist", nameof(companyUserId));
-        }
 
         var notificationId = Guid.NewGuid();
-        var (dateTimeOffset, title, message, notificationTypeId, notificationStatusId, appId, dueData, creatorUserId) = creationData;
+        var (dateTimeOffset, title, message, notificationTypeId, notificationStatusId, appId, dueData, creatorUserId) =
+            creationData;
         CheckEnumValues(notificationTypeId, notificationStatusId);
-        this._portalRepositories.GetInstance<INotificationRepository>().Add(new PortalBackend.PortalEntities.Entities.Notification(notificationId, companyUserId, dateTimeOffset, title, message, notificationTypeId, notificationStatusId)
-        {
-            DueDate = dueData,
-            AppId = appId,
-            CreatorUserId = creatorUserId
-        });
-        await this._portalRepositories.SaveAsync().ConfigureAwait(false);
+        _portalRepositories.GetInstance<INotificationRepository>().Add(
+            new PortalBackend.PortalEntities.Entities.Notification(notificationId, companyUserId, dateTimeOffset, title,
+                message, notificationTypeId, notificationStatusId)
+            {
+                DueDate = dueData,
+                AppId = appId,
+                CreatorUserId = creatorUserId
+            });
+        await _portalRepositories.SaveAsync().ConfigureAwait(false);
         return new NotificationDetailData(notificationId, title, message);
     }
 
     /// <summary>
-    /// Check the enum values if the api receives a int value which is not in the range of the valid values
+    ///     Check the enum values if the api receives a int value which is not in the range of the valid values
     /// </summary>
     /// <param name="notificationTypeId">The notification type</param>
     /// <param name="notificationStatusId">The notification status</param>
-    private static void CheckEnumValues(NotificationTypeId notificationTypeId, NotificationStatusId notificationStatusId)
+    private static void CheckEnumValues(NotificationTypeId notificationTypeId,
+        NotificationStatusId notificationStatusId)
     {
-        if(!Enum.IsDefined(typeof(NotificationTypeId), notificationTypeId))
-        {
+        if (!Enum.IsDefined(typeof(NotificationTypeId), notificationTypeId))
             throw new ArgumentException("notificationType does not exist.", nameof(notificationTypeId));
-        }
 
-        if(!Enum.IsDefined(typeof(NotificationStatusId), notificationStatusId.ToString()))
-        {
+        if (!Enum.IsDefined(typeof(NotificationStatusId), notificationStatusId.ToString()))
             throw new ArgumentException("notificationStatus does not exist.", nameof(notificationStatusId));
-        }
     }
 }
--- src/notification/CatenaX.NetworkServices.Notification.Service/Program.cs
+++ src/notification/CatenaX.NetworkServices.Notification.Service/Program.cs
@@ -1,20 +1,42 @@
+// /********************************************************************************
+//  * Copyright (c) 2021,2022 BMW Group AG
+//  * Copyright (c) 2021,2022 Contributors to the CatenaX (ng) GitHub Organisation.
+//  *
+//  * See the NOTICE file(s) distributed with this work for additional
+//  * information regarding copyright ownership.
+//  *
+//  * This program and the accompanying materials are made available under the
+//  * terms of the Apache License, Version 2.0 which is available at
+//  * https://www.apache.org/licenses/LICENSE-2.0.
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//  * License for the specific language governing permissions and limitations
+//  * under the License.
+//  *
+//  * SPDX-License-Identifier: Apache-2.0
+//  ********************************************************************************/
+
+#region
+
+using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json.Serialization;
 using CatenaX.NetworkServices.Framework.ErrorHandling;
 using CatenaX.NetworkServices.Keycloak.Authentication;
 using CatenaX.NetworkServices.Keycloak.Factory;
 using CatenaX.NetworkServices.Keycloak.Factory.Utils;
+using CatenaX.NetworkServices.Notification.Service.BusinessLogic;
 using CatenaX.NetworkServices.PortalBackend.DBAccess;
 using CatenaX.NetworkServices.PortalBackend.PortalEntities;
-
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.OpenApi.Models;
 
-using System.IdentityModel.Tokens.Jwt;
-using System.Text.Json.Serialization;
-using CatenaX.NetworkServices.Notification.Service.BusinessLogic;
-using Microsoft.EntityFrameworkCore;
+#endregion
 
 var VERSION = "v2";
 var TAG = typeof(Program).Namespace;
@@ -26,70 +48,69 @@ var builder = WebApplication.CreateBuilder(args);
 if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Kubernetes")
 {
     var provider = new PhysicalFileProvider("/app/secrets");
-    builder.Configuration.AddJsonFile(provider, "appsettings.json", optional: false, reloadOnChange: false);
+    builder.Configuration.AddJsonFile(provider, "appsettings.json", false, false);
 }
 
 builder.Services.AddControllers()
-                .AddJsonOptions(options => {
-                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-                });
+    .AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); });
 
-builder.Services.AddSwaggerGen(c => {
-                    c.SwaggerDoc(VERSION, new OpenApiInfo { Title = TAG, Version = VERSION });
-                });
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc(VERSION, new OpenApiInfo {Title = TAG, Version = VERSION}); });
 
-builder.Services.AddAuthentication(x => {
-                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                }).AddJwtBearer(options => {
-                    builder.Configuration.Bind("JwtBearerOptions", options);
-                    if (!options.RequireHttpsMetadata)
-                    {
-                        options.BackchannelHttpHandler = new HttpClientHandler
-                        {
-                            ServerCertificateCustomValidationCallback = (a, b, c, d) => true
-                        };
-                    }
-                });
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    builder.Configuration.Bind("JwtBearerOptions", options);
+    if (!options.RequireHttpsMetadata)
+        options.BackchannelHttpHandler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = (a, b, c, d) => true
+        };
+});
 
 JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
-builder.Services.AddTransient<IAuthorizationHandler,ClaimRequestPathHandler>()
-                    .AddAuthorization(option => {
-                        option.AddPolicy("CheckTenant", policy =>
-                        {
-                            policy.AddRequirements(new ClaimRequestPathRequirement("tenant","tenant"));
-                        });
-                    })
-                    .AddTransient<IHttpContextAccessor,HttpContextAccessor>();
+builder.Services.AddTransient<IAuthorizationHandler, ClaimRequestPathHandler>()
+    .AddAuthorization(option =>
+    {
+        option.AddPolicy("CheckTenant",
+            policy => { policy.AddRequirements(new ClaimRequestPathRequirement("tenant", "tenant")); });
+    })
+    .AddTransient<IHttpContextAccessor, HttpContextAccessor>();
 
 builder.Services.AddTransient<IClaimsTransformation, KeycloakClaimsTransformation>()
-                .Configure<JwtBearerOptions>(options => builder.Configuration.Bind("JwtBearerOptions",options));
-                    
+    .Configure<JwtBearerOptions>(options => builder.Configuration.Bind("JwtBearerOptions", options));
+
 builder.Services.AddTransient<IKeycloakFactory, KeycloakFactory>()
-                .ConfigureKeycloakSettingsMap(builder.Configuration.GetSection("Keycloak"));
+    .ConfigureKeycloakSettingsMap(builder.Configuration.GetSection("Keycloak"));
 
 builder.Services.AddTransient<INotificationBusinessLogic, NotificationBusinessLogic>();
 builder.Services.AddTransient<IPortalRepositories, PortalRepositories>();
 
 builder.Services.AddDbContext<PortalDbContext>(options =>
-                    options.UseNpgsql(builder.Configuration.GetConnectionString("PortalDB")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PortalDB")));
 
 var app = builder.Build();
 
-var debugEnabled = app.Configuration.GetValue<bool?>("DebugEnabled") != null && app.Configuration.GetValue<bool>("DebugEnabled");
+var debugEnabled = app.Configuration.GetValue<bool?>("DebugEnabled") != null &&
+                   app.Configuration.GetValue<bool>("DebugEnabled");
 if (debugEnabled)
 {
     app.UseDeveloperExceptionPage();
     KeycloakUntrustedCertExceptionHandler.ConfigureExceptions(app.Configuration.GetSection("Keycloak"));
 }
+
 FlurlErrorHandler.ConfigureErrorHandler(app.Services.GetRequiredService<ILogger<Program>>(), debugEnabled);
 
 if (app.Configuration.GetValue<bool?>("SwaggerEnabled") != null && app.Configuration.GetValue<bool>("SwaggerEnabled"))
 {
-    app.UseSwagger( c => c.RouteTemplate = "/api/notification/swagger/{documentName}/swagger.{json|yaml}");
-    app.UseSwaggerUI(c => {
-        c.SwaggerEndpoint(string.Format("/api/notification/swagger/{0}/swagger.json",VERSION), string.Format("{0} {1}",TAG,VERSION));
+    app.UseSwagger(c => c.RouteTemplate = "/api/notification/swagger/{documentName}/swagger.{json|yaml}");
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint(string.Format("/api/notification/swagger/{0}/swagger.json", VERSION),
+            string.Format("{0} {1}", TAG, VERSION));
         c.RoutePrefix = "api/notification/swagger";
     });
 }
