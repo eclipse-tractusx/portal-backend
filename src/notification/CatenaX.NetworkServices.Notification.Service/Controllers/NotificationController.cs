9333adc95490d726952aa7f645087a0dc6781236
Philip Schneider <info@philschneider.de>
2022-07-06 11:44:16 +0200

    CPLP-1133 Add File Header

--- src/notification/CatenaX.NetworkServices.Notification.Service/Controllers/NotificationController.cs
+++ src/notification/CatenaX.NetworkServices.Notification.Service/Controllers/NotificationController.cs
@@ -1,4 +1,24 @@
-﻿using CatenaX.NetworkServices.Framework.ErrorHandling;
+﻿// /********************************************************************************
+//  * Copyright (c) 2021,2022 BMW Group AG
+//  * Copyright (c) 2021,2022 Contributors to the CatenaX (ng) GitHub Organisation.
+//  *
+//  * See the NOTICE file(s) distributed with this work for additional
+//  * information regarding copyright ownership.
+//  *
+//  * This program and the accompanying materials are made available under the
+//  * terms of the Apache License, Version 2.0 which is available at
+//  * https://www.apache.org/licenses/LICENSE-2.0.
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//  * License for the specific language governing permissions and limitations
+//  * under the License.
+//  *
+//  * SPDX-License-Identifier: Apache-2.0
+//  ********************************************************************************/
+
+using CatenaX.NetworkServices.Framework.ErrorHandling;
 using CatenaX.NetworkServices.Notification.Service.BusinessLogic;
 using CatenaX.NetworkServices.PortalBackend.DBAccess.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -7,7 +27,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CatenaX.NetworkServices.Notification.Service.Controllers;
 
 /// <summary>
-/// Controller providing actions for creating, displaying and updating notifications.
+///     Controller providing actions for creating, displaying and updating notifications.
 /// </summary>
 [ApiController]
 [Route("api/[controller]")]
@@ -18,7 +38,7 @@ public class NotificationController : ControllerBase
     private readonly INotificationBusinessLogic _logic;
 
     /// <summary>
-    /// Creates a new instance of <see cref="NotificationController"/>
+    ///     Creates a new instance of <see cref="NotificationController" />
     /// </summary>
     /// <param name="logic">The business logic for the notifications</param>
     public NotificationController(INotificationBusinessLogic logic)
@@ -27,9 +47,12 @@ public class NotificationController : ControllerBase
     }
 
     /// <summary>
-    /// Creates a new notification for the given user.
+    ///     Creates a new notification for the given user.
     /// </summary>
-    /// <param name="companyUserId" example="D3B1ECA2-6148-4008-9E6C-C1C2AEA5C645">Id of the user to create the notification for.</param>
+    /// <param name="companyUserId" example="D3B1ECA2-6148-4008-9E6C-C1C2AEA5C645">
+    ///     Id of the user to create the notification
+    ///     for.
+    /// </param>
     /// <param name="data">Contains the information needed to create the notification.</param>
     /// <remarks>Example: POST: /api/notification/D3B1ECA2-6148-4008-9E6C-C1C2AEA5C645</remarks>
     /// <response code="201">Notification was successfully created.</response>
@@ -39,9 +62,11 @@ public class NotificationController : ControllerBase
     [Authorize(Roles = "view_notifications")]
     [ProducesResponseType(typeof(NotificationDetailData), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<NotificationDetailData>> CreateNotification([FromRoute] Guid companyUserId, [FromBody] NotificationCreationData data)
+    public async Task<ActionResult<NotificationDetailData>> CreateNotification([FromRoute] Guid companyUserId,
+        [FromBody] NotificationCreationData data)
     {
         var notificationDetailData = await _logic.CreateNotification(data, companyUserId).ConfigureAwait(false);
-        return CreatedAtRoute(nameof(CreateNotification), new { notificationId = notificationDetailData.Id }, notificationDetailData); // TODO - change the createdAtRoute as soon the get method exists
+        return CreatedAtRoute(nameof(CreateNotification), new {notificationId = notificationDetailData.Id},
+            notificationDetailData); // TODO - change the createdAtRoute as soon the get method exists
     }
 }
