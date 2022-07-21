cb762a205e237916719f05886e503860d0387fde
Philip Schneider <info@philschneider.de>
2022-07-21 11:09:37 +0200

    CPLP-1189 edit null check

--- src/marketplace/CatenaX.NetworkServices.App.Service/BusinessLogic/AppsBusinessLogic.cs
+++ src/marketplace/CatenaX.NetworkServices.App.Service/BusinessLogic/AppsBusinessLogic.cs
@@ -137,7 +137,7 @@ public class AppsBusinessLogic : IAppsBusinessLogic
         await _portalRepositories.SaveAsync().ConfigureAwait(false);
 
         var appDetails = await _portalRepositories.GetInstance<IAppRepository>().GetAppProviderDetailsAsync(appId).ConfigureAwait(false);
-        if(new []{ appDetails.AppName, appDetails.ProviderContactEmail }.Any(d => d is null))
+        if(appDetails.AppName is null || appDetails.ProviderContactEmail is null)
         {
             var nullProperties = new List<string>();
             if (appDetails.AppName is null)
