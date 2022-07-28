337ee2fb0e86bd3667fa7004312b4dd23df22291
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-07-28 16:26:56 +0200

    CPLP-1151 change CompanyUserDetails and CompanyUserAssingedRoleDetails to record

--- src/portalbackend/CatenaX.NetworkServices.PortalBackend.DBAccess/Models/CompanyUserDetails.cs
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.DBAccess/Models/CompanyUserDetails.cs
@@ -3,21 +3,14 @@ using System.Text.Json.Serialization;
 
 namespace CatenaX.NetworkServices.PortalBackend.DBAccess.Models;
 
-public class CompanyUserDetails
+public record CompanyUserDetails(
+    [property: JsonPropertyName("companyUserId")] Guid companyUserId,
+    [property: JsonPropertyName("created")] DateTimeOffset createdAt,
+    [property: JsonPropertyName("bpn")] IEnumerable<string> businessPartnerNumbers,
+    [property: JsonPropertyName("company")] string companyName,
+    [property: JsonPropertyName("status")] CompanyUserStatusId companyUserStatusId,
+    [property: JsonPropertyName("assignedRoles")] IEnumerable<CompanyUserAssignedRoleDetails> assignedRoles)
 {
-    public CompanyUserDetails(Guid companyUserId, DateTimeOffset createdAt, IEnumerable<string> businessPartnerNumbers, string companyName, CompanyUserStatusId companyUserStatusId,IEnumerable<CompanyUserAssignedRoleDetails> assignedRoles)
-    {
-        CompanyUserId = companyUserId;
-        CreatedAt = createdAt;
-        BusinessPartnerNumbers = businessPartnerNumbers;
-        CompanyName = companyName;
-        CompanyUserStatusId = companyUserStatusId;
-        AssignedRoles = assignedRoles;
-    }
-
-    [JsonPropertyName("companyUserId")]
-    public Guid CompanyUserId { get; set; }
-
     [JsonPropertyName("firstName")]
     public string? FirstName { get; set; }
 
@@ -26,35 +19,8 @@ public class CompanyUserDetails
 
     [JsonPropertyName("email")]
     public string? Email { get; set; }
-
-    [JsonPropertyName("bpn")]
-    public IEnumerable<string> BusinessPartnerNumbers { get; set; }
-
-    [JsonPropertyName("created")]
-    public DateTimeOffset CreatedAt { get; set; }
-
-    [JsonPropertyName("company")]
-    public string CompanyName { get; set; }
-
-    [JsonPropertyName("status")]
-    public CompanyUserStatusId CompanyUserStatusId { get; set; }
-
-    [JsonPropertyName("assignedRoles")]
-    public IEnumerable<CompanyUserAssignedRoleDetails> AssignedRoles { get; set; }
 }
 
-public class CompanyUserAssignedRoleDetails
-{
-    public CompanyUserAssignedRoleDetails(Guid appId, IEnumerable<string> roles)
-    {
-        AppId = appId;
-        UserRoles = roles;
-    }
-
-    [JsonPropertyName("appId")]
-    public Guid AppId { get; set; }
-
-    [JsonPropertyName("roles")]
-    public IEnumerable<string> UserRoles { get; set; }
-
-}
+public record CompanyUserAssignedRoleDetails(
+    [property: JsonPropertyName("appId")] Guid appId,
+    [property: JsonPropertyName("roles")] IEnumerable<string> userRoles);
