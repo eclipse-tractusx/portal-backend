c0e7830a55b07a2fe3f2fbb9c345e4f8d29b6e2d
Phil Schneider <info@philschneider.de>
2022-08-01 22:25:56 +0200

    CPLP-1306 extended unit tests

--- tests/administration/CatenaX.NetworkServices.Administration.Service.Tests/BusinessLogic/ConnectorsBusinessLogicTests.cs
+++ tests/administration/CatenaX.NetworkServices.Administration.Service.Tests/BusinessLogic/ConnectorsBusinessLogicTests.cs
@@ -24,6 +24,7 @@ public class ConnectorsBusinessLogicTests
     private static readonly Guid _validHostId = Guid.NewGuid();
     private static readonly Guid _companyWithoutBpnId = Guid.NewGuid();
     private static readonly Guid _invalidCompanyId = Guid.NewGuid();
+    private static readonly Guid _invalidHostId = Guid.NewGuid();
     private static readonly string _accessToken = "validToken";
     private static readonly List<Connector> _connectors = new();
     private readonly ICountryRepository _countryRepository;
@@ -51,48 +52,7 @@ public class ConnectorsBusinessLogicTests
         _options = A.Fake<IOptions<ConnectorsSettings>>();
         _settings = A.Fake<ConnectorsSettings>();
 
-        A.CallTo(() => _countryRepository.CheckCountryExistsByAlpha2CodeAsync(A<string>.That.Matches(x => x.Length == 2)))
-            .Returns(true);
-        A.CallTo(() => _countryRepository.CheckCountryExistsByAlpha2CodeAsync(A<string>.That.Not.Matches(x => x.Length == 2)))
-            .Returns(false);
-
-        A.CallTo(() => _companyRepository.GetConnectorCreationCompanyDataAsync(A<IEnumerable<(Guid companyId, bool bpnRequested)>>.That.Matches(x => x.Count() == 1 && x.First().companyId == _validCompanyId)))
-            .Returns(new List<(Guid CompanyId, string? BusinessPartnerNumber)>
-            {
-                new(_validCompanyId, "VALID")
-            }.ToAsyncEnumerable());
-        A.CallTo(() => _companyRepository.GetConnectorCreationCompanyDataAsync(A<IEnumerable<(Guid companyId, bool bpnRequested)>>.That.Matches(x => x.Count() == 2 && x.Any(y => y.companyId == _validCompanyId) && x.Any(y => y.companyId == _validHostId))))
-            .Returns(new List<(Guid CompanyId, string? BusinessPartnerNumber)>
-            {
-                new(_validCompanyId, "VALID"),
-                new(_validHostId, "VALID_HOST")
-            }.ToAsyncEnumerable());
-        A.CallTo(() => _companyRepository.GetConnectorCreationCompanyDataAsync(A<IEnumerable<(Guid companyId, bool bpnRequested)>>.That.Matches(x => x.Count() == 1 && x.Any(y => y.companyId == _invalidCompanyId))))
-            .Returns(new List<(Guid CompanyId, string? BusinessPartnerNumber)>().ToAsyncEnumerable());
-        A.CallTo(() => _companyRepository.GetConnectorCreationCompanyDataAsync(A<IEnumerable<(Guid companyId, bool bpnRequested)>>.That.Matches(x => x.Count() == 1 && x.Any(y => y.companyId == _companyWithoutBpnId))))
-            .Returns(new List<(Guid CompanyId, string? BusinessPartnerNumber)>
-            {
-                new(_companyWithoutBpnId, null)
-            }.ToAsyncEnumerable());
-
-        A.CallTo(() =>
-                _connectorsRepository.CreateConnector(A<string>._, A<string>._, A<string>._, A<Action<Connector>?>._))
-            .Invokes(x =>
-            {
-                var name = x.Arguments.Get<string>("name")!;
-                var location = x.Arguments.Get<string>("location")!;
-                var connectorUrl = x.Arguments.Get<string>("connectorUrl")!;
-                var action = x.Arguments.Get<Action<Connector?>>("setupOptionalFields");
-
-                var connector = new Connector(Guid.NewGuid(), name, location, connectorUrl);
-                action?.Invoke(connector);
-                _connectors.Add(connector);
-            });
-        
-        A.CallTo(() => _connectorsSdFactoryService.RegisterConnector(A<ConnectorInputModel>._, A<string>.That.Matches(x => x == _accessToken), A<string>._))
-            .ReturnsLazily(() => Task.CompletedTask);
-        A.CallTo(() => _connectorsSdFactoryService.RegisterConnector(A<ConnectorInputModel>._, A<string>.That.Not.Matches(x => x == _accessToken), A<string>._))
-            .Throws(() => new ServiceException("Access to SD factory failed with status code 401"));
+        SetupRepositoryMethods();
 
         A.CallTo(() => _portalRepositories.GetInstance<ICountryRepository>()).Returns(_countryRepository);
         A.CallTo(() => _portalRepositories.GetInstance<ICompanyRepository>()).Returns(_companyRepository);
@@ -164,4 +124,89 @@ public class ConnectorsBusinessLogicTests
         var exception = await Assert.ThrowsAsync<UnexpectedConditionException>(Act);
         exception.Message.Should().Be($"provider company {_companyWithoutBpnId} has no businessPartnerNumber assigned");
     }
+    
+    [Fact]
+    public async Task CreateConnectorAsync_WithInvalid_ThrowsUnexpectedConditionException()
+    {
+        // Arrange
+        var connectorInput = new ConnectorInputModel("connectorName", "http://test.de",
+            ConnectorTypeId.CONNECTOR_AS_A_SERVICE, ConnectorStatusId.ACTIVE, "de", _validCompanyId,
+            _invalidHostId);
+        
+        // Act
+        async Task Act() => await _logic.CreateConnectorAsync(connectorInput, _accessToken).ConfigureAwait(false);
+
+        // Assert
+        var exception = await Assert.ThrowsAsync<UnexpectedConditionException>(Act);
+        exception.Message.Should().Be($"Company {_invalidHostId} does not exist");
+    }
+
+    #region Setup
+
+    private void SetupRepositoryMethods()
+    {
+        A.CallTo(() => _countryRepository.CheckCountryExistsByAlpha2CodeAsync(A<string>.That.Matches(x => x.Length == 2)))
+            .Returns(true);
+        A.CallTo(() => _countryRepository.CheckCountryExistsByAlpha2CodeAsync(A<string>.That.Not.Matches(x => x.Length == 2)))
+            .Returns(false);
+
+        A.CallTo(() => _companyRepository.GetConnectorCreationCompanyDataAsync(
+                A<IEnumerable<(Guid companyId, bool bpnRequested)>>.That.Matches(x =>
+                    x.Count() == 1 && x.First().companyId == _validCompanyId)))
+            .Returns(new List<(Guid CompanyId, string? BusinessPartnerNumber)>
+            {
+                new(_validCompanyId, "VALID")
+            }.ToAsyncEnumerable());
+        A.CallTo(() => _companyRepository.GetConnectorCreationCompanyDataAsync(
+                A<IEnumerable<(Guid companyId, bool bpnRequested)>>.That.Matches(x =>
+                    x.Count() == 2 && x.Any(y => y.companyId == _validCompanyId) && x.Any(y => y.companyId == _validHostId))))
+            .Returns(new List<(Guid CompanyId, string? BusinessPartnerNumber)>
+            {
+                new(_validCompanyId, "VALID"),
+                new(_validHostId, "VALID_HOST")
+            }.ToAsyncEnumerable());
+        A.CallTo(() => _companyRepository.GetConnectorCreationCompanyDataAsync(
+                A<IEnumerable<(Guid companyId, bool bpnRequested)>>.That.Matches(x =>
+                    x.Count() == 1 && x.Any(y => y.companyId == _invalidCompanyId))))
+            .Returns(new List<(Guid CompanyId, string? BusinessPartnerNumber)>().ToAsyncEnumerable());
+        A.CallTo(() => _companyRepository.GetConnectorCreationCompanyDataAsync(
+                A<IEnumerable<(Guid companyId, bool bpnRequested)>>.That.Matches(x =>
+                    x.Count() == 1 && x.Any(y => y.companyId == _companyWithoutBpnId))))
+            .Returns(new List<(Guid CompanyId, string? BusinessPartnerNumber)>
+            {
+                new(_companyWithoutBpnId, null)
+            }.ToAsyncEnumerable());
+        A.CallTo(() => _companyRepository.GetConnectorCreationCompanyDataAsync(
+                A<IEnumerable<(Guid companyId, bool bpnRequested)>>.That.Matches(x =>
+                    x.Count() == 2 && x.Any(y => y.companyId == _validCompanyId) && x.Any(y => y.companyId == _invalidHostId))))
+            .Returns(new List<(Guid CompanyId, string? BusinessPartnerNumber)>
+            {
+                new(_validCompanyId, "VALID")
+            }.ToAsyncEnumerable());
+
+        A.CallTo(() =>
+                _connectorsRepository.CreateConnector(A<string>._, A<string>._, A<string>._, A<Action<Connector>?>._))
+            .Invokes(x =>
+            {
+                var name = x.Arguments.Get<string>("name")!;
+                var location = x.Arguments.Get<string>("location")!;
+                var connectorUrl = x.Arguments.Get<string>("connectorUrl")!;
+                var action = x.Arguments.Get<Action<Connector?>>("setupOptionalFields");
+
+                var connector = new Connector(Guid.NewGuid(), name, location, connectorUrl);
+                action?.Invoke(connector);
+                _connectors.Add(connector);
+            });
+
+        A.CallTo(() =>
+                _connectorsSdFactoryService.RegisterConnector(A<ConnectorInputModel>._, A<string>.That.Matches(x => x == _accessToken),
+                    A<string>._))
+            .ReturnsLazily(() => Task.CompletedTask);
+        A.CallTo(() =>
+                _connectorsSdFactoryService.RegisterConnector(A<ConnectorInputModel>._, A<string>.That.Not.Matches(x => x == _accessToken),
+                    A<string>._))
+            .Throws(() => new ServiceException("Access to SD factory failed with status code 401"));
+    }
+
+    #endregion
 }
