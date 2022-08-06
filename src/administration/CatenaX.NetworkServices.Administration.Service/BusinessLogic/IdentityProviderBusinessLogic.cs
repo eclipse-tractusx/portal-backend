83140032fd3ee688dd48821ee0a2afd23e4e426a
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-08-06 20:18:35 +0200

    CPLP-472 license-headers

--- src/administration/CatenaX.NetworkServices.Administration.Service/BusinessLogic/IdentityProviderBusinessLogic.cs
+++ src/administration/CatenaX.NetworkServices.Administration.Service/BusinessLogic/IdentityProviderBusinessLogic.cs
@@ -1,3 +1,23 @@
+/********************************************************************************
+ * Copyright (c) 2021,2022 BMW Group AG
+ * Copyright (c) 2021,2022 Contributors to the CatenaX (ng) GitHub Organisation.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
 using CatenaX.NetworkServices.Administration.Service.Models;
 using CatenaX.NetworkServices.Framework.ErrorHandling;
 using CatenaX.NetworkServices.Keycloak.ErrorHandling;
@@ -214,6 +234,7 @@ public class IdentityProviderBusinessLogic : IIdentityProviderBusinessLogic
                     }
             };
     }
+
     private async Task<IdentityProviderDetails> GetCentralIdentityProviderDetailsSAMLAsync(Guid identityProviderId, string alias)
     {
         var identityProviderDataSAML = await _provisioningManager.GetCentralIdentityProviderDataSAMLAsync(alias).ConfigureAwait(false);
@@ -230,7 +251,6 @@ public class IdentityProviderBusinessLogic : IIdentityProviderBusinessLogic
                     identityProviderDataSAML.SingleSignOnServiceUrl)
             };
     }
-
 
     public async Task<UserIdentityProviderLinkData> CreateOwnCompanyUserIdentityProviderLinkDataAsync(Guid companyUserId, UserIdentityProviderLinkData identityProviderLinkData, string iamUserId)
     {
