b22f0eb2bcdc09b9841e33e16a6fe8e4db168af7
Philip Schneider <info@philschneider.de>
2022-08-08 14:01:41 +0200

    CPLP-1248 add migration for sales manager and requester

---
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.Migrations/Migrations/20220808102805_CPLP-1248-app-notification.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatenaX.NetworkServices.PortalBackend.Migrations.Migrations
+{
+    public partial class CPLP1248appnotification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "requester_id",
+                schema: "portal",
+                table: "company_assigned_apps",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "gen_random_uuid()");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "sales_manager_id",
+                schema: "portal",
+                table: "apps",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "gen_random_uuid()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "sales_manager_id",
+                schema: "portal",
+                table: "apps",
+                type: "uuid",
+                nullable: false);
+            
+            migrationBuilder.AlterColumn<Guid>(
+                name: "requester_id",
+                schema: "portal",
+                table: "company_assigned_apps",
+                type: "uuid",
+                nullable: false);
+            
+            migrationBuilder.CreateIndex(
+                name: "ix_apps_sales_manager_id",
+                schema: "portal",
+                table: "apps",
+                column: "sales_manager_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_apps_company_users_sales_manager_id",
+                schema: "portal",
+                table: "apps",
+                column: "sales_manager_id",
+                principalSchema: "portal",
+                principalTable: "company_users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_apps_company_users_sales_manager_id",
+                schema: "portal",
+                table: "apps");
+
+            migrationBuilder.DropIndex(
+                name: "ix_apps_sales_manager_id",
+                schema: "portal",
+                table: "apps");
+
+            migrationBuilder.DropColumn(
+                name: "requester_id",
+                schema: "portal",
+                table: "company_assigned_apps");
+
+            migrationBuilder.DropColumn(
+                name: "sales_manager_id",
+                schema: "portal",
+                table: "apps");
+        }
+    }
+}
