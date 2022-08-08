814893d45ba32d9d431396ddfe4dd1ebe77dc796
Philip Schneider <info@philschneider.de>
2022-08-08 11:03:38 +0200

    CPLP-1253 merge audit migrations

--- src/portalbackend/CatenaX.NetworkServices.PortalBackend.Migrations/Migrations/20220808081248_CPLP-1254-db-audit.cs
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.Migrations/Migrations/20220808081248_CPLP-1254-db-audit.cs
@@ -17,6 +17,47 @@ namespace CatenaX.NetworkServices.PortalBackend.Migrations.Migrations
                 type: "uuid",
                 nullable: true);
 
+            migrationBuilder.AddColumn<Guid>(
+                name: "id",
+                schema: "portal",
+                table: "company_assigned_apps",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "gen_random_uuid()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "portal",
+                table: "company_assigned_apps",
+                type: "uuid",
+                nullable: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "last_editor_id",
+                schema: "portal",
+                table: "company_assigned_apps",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "audit_company_assigned_apps_cplp_1254_db_audit",
+                schema: "portal",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    audit_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    date_last_changed = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    audit_operation_id = table.Column<int>(type: "integer", nullable: false),
+                    last_editor_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    company_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    app_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    app_subscription_status_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_audit_company_assigned_apps_cplp_1254_db_audit", x => x.id);
+                });
+
             migrationBuilder.CreateTable(
                 name: "audit_company_users_cplp_1254_db_audit",
                 schema: "portal",
@@ -78,11 +119,17 @@ namespace CatenaX.NetworkServices.PortalBackend.Migrations.Migrations
                 column: "company_user_status_id");
             
             migrationBuilder.AddAuditTrigger<AuditCompanyUser>("cplp_1254_db_audit");
+            migrationBuilder.AddAuditTrigger<AuditCompanyAssignedApp>("cplp_1254_db_audit");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropAuditTrigger<AuditCompanyUser>();
+            migrationBuilder.DropAuditTrigger<AuditCompanyAssignedApp>();
+
+            migrationBuilder.DropTable(
+                name: "audit_company_assigned_apps_cplp_1254_db_audit",
+                schema: "portal");
 
             migrationBuilder.DropTable(
                 name: "audit_company_users_cplp_1254_db_audit",
@@ -96,6 +143,16 @@ namespace CatenaX.NetworkServices.PortalBackend.Migrations.Migrations
                 name: "last_editor_id",
                 schema: "portal",
                 table: "company_users");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                schema: "portal",
+                table: "company_assigned_apps");
+
+            migrationBuilder.DropColumn(
+                name: "last_editor_id",
+                schema: "portal",
+                table: "company_assigned_apps");
         }
     }
 }
