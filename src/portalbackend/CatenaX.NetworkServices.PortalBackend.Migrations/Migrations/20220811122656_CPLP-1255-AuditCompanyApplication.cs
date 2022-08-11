5f2204e6b681f056acddead91262779d09aaa5dd
qxz2mqe <md-majid.akhter@partner.bmw.in>
2022-08-11 18:54:24 +0530

    CPLP-1255 audit company application

---
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.Migrations/Migrations/20220811122656_CPLP-1255-AuditCompanyApplication.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using CatenaX.NetworkServices.PortalBackend.PortalEntities.AuditEntities;
+
+#nullable disable
+
+namespace CatenaX.NetworkServices.PortalBackend.Migrations.Migrations
+{
+    public partial class CPLP1255AuditCompanyApplication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "last_editor_id",
+                schema: "portal",
+                table: "company_applications",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "audit_company_applications_cplp_1255_audit_company_applications",
+                schema: "portal",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    audit_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    audit_operation_id = table.Column<int>(type: "integer", nullable: false),
+                    date_last_changed = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    date_created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    application_status_id = table.Column<int>(type: "integer", nullable: false),
+                    company_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    last_editor_id = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_audit_company_applications_cplp_1255_audit_company_applicat", x => x.id);
+                });
+            migrationBuilder.AddAuditTrigger<AuditCompanyApplication>("cplp_1255_audit_company_applications");   
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropAuditTrigger<AuditCompanyApplication>();
+            migrationBuilder.DropTable(
+                name: "audit_company_applications_cplp_1255_audit_company_applications",
+                schema: "portal");
+
+            migrationBuilder.DropColumn(
+                name: "last_editor_id",
+                schema: "portal",
+                table: "company_applications");
+        }
+    }
+}
