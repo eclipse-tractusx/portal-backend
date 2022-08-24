eb72a16022c559ff32e15ee10d827b9dfc5127a7
Akshay Chaudhari <akshay-dilip.chaudhari@partner.bmw.in>
2022-08-24 13:41:36 +0530

    cplp-1399- add missing company role id.

--- src/portalbackend/CatenaX.NetworkServices.PortalBackend.PortalEntities/Enums/CompanyRoleId.cs
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.PortalEntities/Enums/CompanyRoleId.cs
@@ -3,5 +3,6 @@ namespace CatenaX.NetworkServices.PortalBackend.PortalEntities.Enums;
 public enum CompanyRoleId : int
 {
     ACTIVE_PARTICIPANT = 1,
-    APP_PROVIDER = 2
+    APP_PROVIDER = 2,
+    SERVICE_PROVIDER = 3
 }
