4b275d54f5e983e175d73af2a1f4ab77ba5de0ec
Philip Schneider <info@philschneider.de>
2022-09-22 12:39:25 +0200

    CPLP-1422 add migration

---
+++ src/portalbackend/CatenaX.NetworkServices.PortalBackend.Migrations/Migrations/20220922092106_CPLP-1422-AddDocumentToConnector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatenaX.NetworkServices.PortalBackend.Migrations.Migrations
+{
+    public partial class CPLP1422AddDocumentToConnector : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "self_description_document_id",
+                schema: "portal",
+                table: "connectors",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_connectors_self_description_document_id",
+                schema: "portal",
+                table: "connectors",
+                column: "self_description_document_id",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_connectors_documents_self_description_document_id",
+                schema: "portal",
+                table: "connectors",
+                column: "self_description_document_id",
+                principalSchema: "portal",
+                principalTable: "documents",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_connectors_documents_self_description_document_id",
+                schema: "portal",
+                table: "connectors");
+
+            migrationBuilder.DropIndex(
+                name: "ix_connectors_self_description_document_id",
+                schema: "portal",
+                table: "connectors");
+
+            migrationBuilder.DropColumn(
+                name: "self_description_document_id",
+                schema: "portal",
+                table: "connectors");
+        }
+    }
+}
