70536a23d58d231e1c42e2eed75e0be63689559d
Philip Schneider <info@philschneider.de>
2022-09-22 14:11:42 +0200

    CPLP-1422 add unit test

---
+++ tests/shared/CatenaX.NetworkServices.Tests.Shared/FormFileHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace CatenaX.NetworkServices.Tests.Shared;
+
+public static class FormFileHelper
+{
+    public static IFormFile GetFormFile(string content, string fileName, string contentType)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(content);
+        writer.Flush();
+        stream.Position = 0;
+
+        var headers = new HeaderDictionary {{HeaderNames.ContentType, contentType}};
+        return new FormFile(stream, 0, stream.Length, "id_from_form", fileName)
+        {
+            Headers = headers 
+        };
+    }
+}
