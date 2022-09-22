c4acafcf1f9cf8538019c2eddc4b8c8efe5a51cc
Philip Schneider <info@philschneider.de>
2022-09-22 13:17:08 +0200

    CPLP-1422 changed secret

--- tests/administration/CatenaX.NetworkServices.Administration.Service.Tests/BusinessLogic/ConnectorsSdFactoryServiceTests.cs
+++ tests/administration/CatenaX.NetworkServices.Administration.Service.Tests/BusinessLogic/ConnectorsSdFactoryServiceTests.cs
@@ -87,13 +87,13 @@ public class ConnectorsSdFactoryServiceTests
             'created': '2022-06-14T11:10:13Z',
             'proofPurpose': 'assertionMethod',
             'verificationMethod': 'did:indy:idunion:test:123456789#key-1',
-            'jws': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.dyt0CoTl4WoVjAHI9Q_CwSKhl6d_9rhM3NrXuJttkao'
+            'jws': 'this-is-a-super-secret-secret-not'
           }
         }";
         var httpMessageHandlerMock = new HttpMessageHandlerMock(HttpStatusCode.OK, FormContent(contentJson, "application/vc+ld+json"));
         var httpClient = new HttpClient(httpMessageHandlerMock);
         var connectorInputModel = new ConnectorInputModel("Connec Tor", "https://connect-tor.com", ConnectorTypeId.COMPANY_CONNECTOR, ConnectorStatusId.ACTIVE, "de", Guid.NewGuid(), Guid.NewGuid());
-        var accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.dyt0CoTl4WoVjAHI9Q_CwSKhl6d_9rhM3NrXuJttkao";
+        var accessToken = "this-is-a-super-secret-secret-not";
         var bpn = "BPNL000000000009";
         A.CallTo(() => _httpClientFactory.CreateClient(A<string>._)).Returns(httpClient);
 
