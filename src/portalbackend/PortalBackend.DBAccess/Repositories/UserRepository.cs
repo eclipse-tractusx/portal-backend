a906beae34b958c28447247f9300d94753fb3a2c
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-09-29 12:42:01 +0200

    CPLP-1480 relocate position in file to avoid conflicts

--- src/portalbackend/PortalBackend.DBAccess/Repositories/UserRepository.cs
+++ src/portalbackend/PortalBackend.DBAccess/Repositories/UserRepository.cs
@@ -134,6 +134,17 @@ public class UserRepository : IUserRepository
                 companyUser.Email))
             .SingleOrDefaultAsync();
 
+    public IAsyncEnumerable<string> GetMatchingCompanyIamUsersByNameEmail(string firstName, string lastName, string email, Guid companyId) =>
+        _dbContext.CompanyUsers
+            .AsNoTracking()
+            .Where(companyUser =>
+                companyUser.CompanyId == companyId &&
+                (companyUser.Email == email ||
+                 companyUser.Firstname == firstName ||
+                 companyUser.Lastname == lastName ))
+            .Select(companyUser => companyUser.IamUser!.UserEntityId)
+            .AsAsyncEnumerable();
+
     public Task<(Guid companyId, Guid companyUserId)> GetOwnCompanAndCompanyUseryId(string iamUserId) =>
         _dbContext.IamUsers
             .AsNoTracking()
@@ -147,17 +158,6 @@ public class UserRepository : IUserRepository
             .Where(iamUser => iamUser.UserEntityId == iamUserId)
             .Select(iamUser => new ValueTuple<Guid, Guid, string, string?>(iamUser.CompanyUser!.CompanyId, iamUser.CompanyUserId, iamUser.CompanyUser!.Company!.Name, iamUser.CompanyUser!.Email))
             .SingleOrDefaultAsync();
-
-    public IAsyncEnumerable<string> GetMatchingCompanyIamUsersByNameEmail(string firstName, string lastName, string email, Guid companyId) =>
-        _dbContext.CompanyUsers
-            .AsNoTracking()
-            .Where(companyUser =>
-                companyUser.CompanyId == companyId &&
-                (companyUser.Email == email ||
-                 companyUser.Firstname == firstName ||
-                 companyUser.Lastname == lastName ))
-            .Select(companyUser => companyUser.IamUser!.UserEntityId)
-            .AsAsyncEnumerable();
 
     public Task<bool> IsOwnCompanyUserWithEmailExisting(string email, string adminUserId) =>
         _dbContext.IamUsers
