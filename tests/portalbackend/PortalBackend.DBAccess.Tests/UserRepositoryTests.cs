a5728a93aa0ff551a237f3d94f93d6e4610fac7d
Philip Schneider <info@philschneider.de>
2022-09-30 09:45:34 +0200

    CPLP-1491 move GetOwnCompanyAppUsersUntrackedAsync

---
+++ tests/portalbackend/PortalBackend.DBAccess.Tests/UserRepositoryTests.cs
@@ -0,0 +1,155 @@
+/********************************************************************************
+ * Copyright (c) 2021,2022 BMW Group AG
+ * Copyright (c) 2021,2022 Contributors to the CatenaX (ng) GitHub Organisation.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+using AutoFixture;
+using AutoFixture.AutoFakeItEasy;
+using Org.CatenaX.Ng.Portal.Backend.PortalBackend.DBAccess.Repositories;
+using Org.CatenaX.Ng.Portal.Backend.PortalBackend.PortalEntities;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Org.CatenaX.Ng.Portal.Backend.PortalBackend.DBAccess.Tests.Setup;
+using Xunit;
+using Xunit.Extensions.AssemblyFixture;
+
+namespace Org.CatenaX.Ng.Portal.Backend.PortalBackend.DBAccess.Tests;
+
+/// <summary>
+/// Tests the functionality of the <see cref="UserRepository"/>
+/// </summary>
+public class UserRepositoryTests : IAssemblyFixture<TestDbFixture>
+{
+    private readonly IFixture _fixture;
+    private readonly TestDbFixture _dbTestDbFixture;
+    private const string ValidIamUserId = "623770c5-cf38-4b9f-9a35-f8b9ae972e2d";
+    private readonly Guid _validOfferId = new("99C5FD12-8085-4DE2-ABFD-215E1EE4BAA4");
+
+    public UserRepositoryTests(TestDbFixture testDbFixture)
+    {
+        _fixture = new Fixture().Customize(new AutoFakeItEasyCustomization { ConfigureMembers = true });
+        _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+            .ForEach(b => _fixture.Behaviors.Remove(b));
+
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        _dbTestDbFixture = testDbFixture;
+    }
+
+    #region GetOwnCompanAndCompanyUseryIdWithCompanyNameAndUserEmailAsync
+
+    [Fact]
+    public async Task GetOwnCompanAndCompanyUseryIdWithCompanyNameAndUserEmailAsync_WithValidIamUser_ReturnsExpectedResult()
+    {
+        // Arrange
+        var (sut, _) = await CreateSut().ConfigureAwait(false);
+
+        // Act
+        var result = await sut.GetOwnCompanAndCompanyUseryIdWithCompanyNameAndUserEmailAsync(ValidIamUserId).ConfigureAwait(false);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.companyUserId.Should().Be(new Guid("ac1cf001-7fbc-1f2f-817f-bce058020000"));
+        result.companyName.Should().Be("Catena-X");
+    }
+
+    [Fact]
+    public async Task GetOwnCompanAndCompanyUseryIdWithCompanyNameAndUserEmailAsync_WithNotExistingIamUser_ReturnsDefault()
+    {
+        // Arrange
+        var (sut, _) = await CreateSut().ConfigureAwait(false);
+
+        // Act
+        var result = await sut.GetOwnCompanAndCompanyUseryIdWithCompanyNameAndUserEmailAsync(Guid.NewGuid().ToString()).ConfigureAwait(false);
+
+        // Assert
+        (result == default).Should().BeTrue();
+    }
+
+    #endregion
+
+    #region GetOwnCompanAndCompanyUseryId
+
+    [Fact]
+    public async Task GetOwnCompanAndCompanyUseryId_WithValidIamUser_ReturnsExpectedResult()
+    {
+        // Arrange
+        var (sut, _) = await CreateSut().ConfigureAwait(false);
+
+        // Act
+        var result = await sut.GetOwnCompanyAndCompanyUserId(ValidIamUserId).ConfigureAwait(false);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.companyUserId.Should().Be(new Guid("ac1cf001-7fbc-1f2f-817f-bce058020000"));
+    }
+
+    [Fact]
+    public async Task GetOwnCompanAndCompanyUseryId_WithNotExistingIamUser_ReturnsDefault()
+    {
+        // Arrange
+        var (sut, _) = await CreateSut().ConfigureAwait(false);
+
+        // Act
+        var result = await sut.GetOwnCompanyAndCompanyUserId(Guid.NewGuid().ToString()).ConfigureAwait(false);
+
+        // Assert
+        (result == default).Should().BeTrue();
+    }
+
+    #endregion
+
+    #region Get own company app users
+
+    [Fact]
+    public async Task GetOwnCompanyAppUsersUntrackedAsync_WithValidIamUser_ReturnsExpectedResult()
+    {
+        // Arrange
+        var (sut, _) = await CreateSut().ConfigureAwait(false);
+
+        // Act
+        var result = await sut.GetOwnCompanyAppUsersUntrackedAsync(_validOfferId, ValidIamUserId).ToListAsync().ConfigureAwait(false);
+
+        // Assert
+        result.Should().NotBeEmpty();
+        result.Should().HaveCount(6);
+    }
+
+    [Fact]
+    public async Task GetOwnCompanyAppUsersUntrackedAsync_WithNotExistingIamUser_ReturnsDefault()
+    {
+        // Arrange
+        var (sut, _) = await CreateSut().ConfigureAwait(false);
+
+        // Act
+        var result = await sut.GetOwnCompanyAppUsersUntrackedAsync(_validOfferId, Guid.NewGuid().ToString()).ToListAsync().ConfigureAwait(false);
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+
+    #endregion
+
+    private async Task<(UserRepository, PortalDbContext)> CreateSut()
+    {
+        var context = await _dbTestDbFixture.GetPortalDbContext().ConfigureAwait(false);
+        _fixture.Inject(context);
+        var sut = _fixture.Create<UserRepository>();
+        return (sut, context);
+    }
+}
