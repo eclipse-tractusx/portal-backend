8cc40ed7594c1555dd19b33789f78c63a0fb4972
Philip Schneider <info@philschneider.de>
2022-10-05 16:01:17 +0200

    CPLP-1417 added migration

--- src/portalbackend/PortalBackend.PortalEntities/Entities/CompanyUserAssignedRole.cs
+++ src/portalbackend/PortalBackend.PortalEntities/Entities/CompanyUserAssignedRole.cs
@@ -23,7 +23,7 @@ using Org.CatenaX.Ng.Portal.Backend.PortalBackend.PortalEntities.Auditing;
 
 namespace Org.CatenaX.Ng.Portal.Backend.PortalBackend.PortalEntities.Entities;
 
-[AuditEntityV1(typeof(AuditCompanyUserAssignedRole20221005))]
+[AuditEntityV1(typeof(AuditCompanyUserAssignedRole202210052))]
 public class CompanyUserAssignedRole : IAuditableV1
 {
     public CompanyUserAssignedRole(Guid companyUserId, Guid userRoleId)
