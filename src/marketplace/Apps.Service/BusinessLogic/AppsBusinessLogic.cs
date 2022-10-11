29f15fa771e31beab4ed011e2734aefb052e4b91
Philip Schneider <info@philschneider.de>
2022-10-11 17:09:00 +0200

    CPLP-1660 add autosetup for apps

--- src/marketplace/Apps.Service/BusinessLogic/AppsBusinessLogic.cs
+++ src/marketplace/Apps.Service/BusinessLogic/AppsBusinessLogic.cs
@@ -31,6 +31,8 @@ using Org.CatenaX.Ng.Portal.Backend.PortalBackend.PortalEntities.Entities;
 using Org.CatenaX.Ng.Portal.Backend.PortalBackend.PortalEntities.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Org.CatenaX.Ng.Portal.Backend.Offers.Library.Models;
+using Org.CatenaX.Ng.Portal.Backend.Offers.Library.Service;
 
 namespace Org.CatenaX.Ng.Portal.Backend.Apps.Service.BusinessLogic;
 
@@ -43,6 +45,7 @@ public class AppsBusinessLogic : IAppsBusinessLogic
     private readonly IMailingService _mailingService;
     private readonly INotificationService _notificationService;
     private readonly AppsSettings _settings;
+    private readonly IOfferService _offerService;
 
     /// <summary>
     /// Constructor.
@@ -50,12 +53,14 @@ public class AppsBusinessLogic : IAppsBusinessLogic
     /// <param name="portalRepositories">Factory to access the repositories</param>
     /// <param name="mailingService">Mail service.</param>
     /// <param name="notificationService">Notification service.</param>
+    /// <param name="offerService">Offer service</param>
     /// <param name="settings">Settings</param>
-    public AppsBusinessLogic(IPortalRepositories portalRepositories, IMailingService mailingService, INotificationService notificationService, IOptions<AppsSettings> settings)
+    public AppsBusinessLogic(IPortalRepositories portalRepositories, IMailingService mailingService, INotificationService notificationService, IOfferService offerService, IOptions<AppsSettings> settings)
     {
         _portalRepositories = portalRepositories;
         _mailingService = mailingService;
         _notificationService = notificationService;
+        _offerService = offerService;
         _settings = settings.Value;
     }
 
@@ -467,4 +472,8 @@ public class AppsBusinessLogic : IAppsBusinessLogic
                         app.ThumbnailUrl))
                     .AsAsyncEnumerable()));
     }
+     
+     /// <inheritdoc />
+     public Task<OfferAutoSetupResponseData> AutoSetupAppAsync(OfferAutoSetupData data, string iamUserId) =>
+         _offerService.AutoSetupServiceAsync(data, _settings.ServiceAccountRoles, _settings.CompanyAdminRoles, iamUserId, OfferTypeId.APP);
 }
