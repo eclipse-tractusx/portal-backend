2aa5fdddfa702eccea89ba0bfbc5635f6daaa5f6
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-10-12 11:21:19 +0200

    CPLP-1227 revert changes to test.shared dependencies

--- tests/shared/Tests.Shared/IntegrationTests/EndpointSetup/Paths.cs
+++ tests/shared/Tests.Shared/IntegrationTests/EndpointSetup/Paths.cs
@@ -18,7 +18,7 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-namespace Org.CatenaX.Ng.Portal.Backend.Tests.Shared.IntegrationTests.EnpointSetup;
+namespace Org.CatenaX.Ng.Portal.Backend.Tests.Shared.IntegrationTests.EndpointSetup;
 
 public static class Paths
 {
