09772ef4237eb2e7f81bf9087095de14505a65c3
Philip Schneider <info@philschneider.de>
2022-10-14 08:05:09 +0200

    CPLP-1417 return not assigned roles

--- src/administration/Administration.Service/BusinessLogic/IUserBusinessLogic.cs
+++ src/administration/Administration.Service/BusinessLogic/IUserBusinessLogic.cs
@@ -52,7 +52,7 @@ public interface IUserBusinessLogic
     /// <param name="userRoleInfo">User and Role Information like CompanyUser Id and Role Name</param>
     /// <param name="adminUserId">Admin User Id</param>
     /// <returns>messages</returns>
-    Task ModifyUserRoleAsync(Guid appId, UserRoleInfo userRoleInfo, string adminUserId);
+    Task<IEnumerable<UserRoleWithId>> ModifyUserRoleAsync(Guid appId, UserRoleInfo userRoleInfo, string adminUserId);
 
     Task<int> DeleteOwnUserBusinessPartnerNumbersAsync(Guid companyUserId, string businessPartnerNumber, string adminUserId);
 }
