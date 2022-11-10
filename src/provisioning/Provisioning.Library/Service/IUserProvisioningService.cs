3e8dd8feb981e929069253e67143d5dc9b7631b1
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-11-10 12:07:05 +0100

    CPLP-1721 add optional parameter 'displayName' to identityprovider creation

--- src/provisioning/Provisioning.Library/Service/IUserProvisioningService.cs
+++ src/provisioning/Provisioning.Library/Service/IUserProvisioningService.cs
@@ -28,6 +28,7 @@ public interface IUserProvisioningService
     IAsyncEnumerable<(Guid CompanyUserId, string UserName, string? Password, Exception? Error)> CreateOwnCompanyIdpUsersAsync(CompanyNameIdpAliasData companyNameIdpAliasData, IAsyncEnumerable<UserCreationRoleDataIdpInfo> userCreationInfos, CancellationToken cancellationToken = default);
     Task<CompanyNameIdpAliasData> GetCompanyNameIdpAliasData(Guid identityProviderId, string iamUserId);
     Task<CompanyNameIdpAliasData> GetCompanyNameSharedIdpAliasData(string iamUserId);
+    Task<string> GetIdentityProviderDisplayName(string idpAlias);
     IAsyncEnumerable<UserRoleData> GetRoleDatas(IDictionary<string,IEnumerable<string>> clientRoles);
     Task<IEnumerable<UserRoleData>> GetOwnCompanyPortalRoleDatas(string clientId, IEnumerable<string> roles, string iamUserId);
 }
