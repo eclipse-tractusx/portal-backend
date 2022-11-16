e9d638b853e53e2b67432eb445b03be23ed95443
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-11-16 16:39:36 +0100

    CPLP/1803 cleanup

--- src/portalbackend/PortalBackend.PortalEntities/PortalDbContext.cs
+++ src/portalbackend/PortalBackend.PortalEntities/PortalDbContext.cs
@@ -532,7 +532,7 @@ public class PortalDbContext : DbContext
         modelBuilder.Entity<ProviderCompanyDetail>(entity =>
         {
             entity.HasOne(e => e.Company)
-                .WithOne(e => e.ServiceProviderCompanyDetail)
+                .WithOne(e => e.ProviderCompanyDetail)
                 .OnDelete(DeleteBehavior.ClientSetNull);
         });
 
