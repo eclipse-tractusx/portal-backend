5cd434663d5ebfd9218f34a817ba7fe730d7393b
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-11-18 08:48:46 +0100

    CPLP-1355 provisioning initial migration

---
+++ src/provisioning/Provisioning.Migrations/ProvisioningDbContextFactory.cs
@@ -0,0 +1,44 @@
+/********************************************************************************
+ * Copyright (c) 2021,2022 BMW Group AG
+ * Copyright (c) 2021,2022 Contributors to the CatenaX (ng) GitHub Organisation.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Org.CatenaX.Ng.Portal.Backend.Provisioning.ProvisioningEntities;
+
+public class ProvisioningDbContextFactory : IDesignTimeDbContextFactory<ProvisioningDbContext>
+{
+    public ProvisioningDbContext CreateDbContext(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("secrets/appsettings.json", true) // Only used in k8s deployment
+            .AddUserSecrets(Assembly.GetExecutingAssembly())
+            .Build();
+        var optionsBuilder = new DbContextOptionsBuilder<ProvisioningDbContext>();
+        optionsBuilder.UseNpgsql(
+            config.GetConnectionString("ProvisioningDb"),
+            x => x.MigrationsAssembly(typeof(ProvisioningDbContextFactory).Assembly.GetName().Name)
+                  .MigrationsHistoryTable("__efmigrations_history_provisioning", "public"));
+
+        return new ProvisioningDbContext(optionsBuilder.Options);
+    }
+}
---
+++ src/provisioning/Provisioning.Migrations/readme.md
@@ -0,0 +1,21 @@
+﻿# Creating Migrations
+After changing anything related to ProvisioningDbContext, make sure your user secrets contain a valid connection string (template in appsettings.json, connection will be tested, nothing will be written). 
+Then open a command line in this project's folder and execute the following instruction:
+
+`dotnet ef migrations add <NAME>` 
+
+where \<NAME> is a placeholder which should be filled with CamelCase description of what changed in the database schema.
+
+Example: `dotnet ef migrations add AddNameToCustomerTable`
+
+# Applying Migrations to database
+To apply one or multiple migrations to a database, there are multiple options available.
+First, make sure you have the proper connection string set either in appsettings or user secrets. The user credentials need to have permissions to change the schema.
+Afterwards choose one of the following:
+- `dotnet ef migrations bundle [-r linux-x64]` to create `efbundle.exe` which then needs to be executed
+- `dotnet ef database update`
+- Execute the project's program
+
+By default, the database will be updated incrementally to the most recent migration. Applying a migration will create a database if none exists with the specified name on the database server.
+
+**Never push efbundle.exe or any secrets in appsettings to Git!**
