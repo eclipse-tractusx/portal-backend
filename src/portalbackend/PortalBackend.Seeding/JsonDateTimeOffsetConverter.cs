2e913378668c1c9d35bcc753210242f43d0eb25e
Phil Schneider <info@philschneider.de>
2022-12-09 11:52:19 +0100

    CPLP-1042 working address seeder

---
+++ src/portalbackend/PortalBackend.Seeding/JsonDateTimeOffsetConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Seeding;
+
+public class JsonDateTimeOffsetConverter: JsonConverter<DateTimeOffset>
+{
+        public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (!reader.TryGetDateTimeOffset(out DateTimeOffset value))
+            {
+                value = DateTimeOffset.Parse(reader.GetString()!);
+            }
+
+            return value;
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString("O"));
+        }
+}
