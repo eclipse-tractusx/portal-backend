48c4d0d76028d4112076c83f5575e8416402ff11
Phil Schneider <info@philschneider.de>
2022-12-09 11:07:49 +0100

    CPLP-1042 modify address seeder

---
+++ src/portalbackend/PortalBackend.Seeding/SeederHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Text.Json;
+
+namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Seeding;
+
+public static class SeederHelper
+{
+    public static async Task<IList<T>?> GetSeedData<T>(CancellationToken cancellationToken) where T : class
+    {
+        var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var path = $"{location}/Seeder/Data/{nameof(T)}.json";
+        if (!File.Exists(path))
+        {
+            return null;
+        }
+
+        var data = await File.ReadAllTextAsync(path, cancellationToken);
+        return JsonSerializer.Deserialize<List<T>>(data);
+    }
+}
