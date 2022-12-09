3bcdde2b082a69dff34c455f8f7243cd4a871c50
Phil Schneider <info@philschneider.de>
2022-12-09 14:36:52 +0100

    CPLP-1042 Add Header

--- src/portalbackend/PortalBackend.Migrations/Seeder/AddressSeeder.cs
+++ src/portalbackend/PortalBackend.Migrations/Seeder/AddressSeeder.cs
@@ -1,7 +1,24 @@
-using System.Reflection;
-using System.Text.Json;
+/********************************************************************************
+ * Copyright (c) 2021,2022 BMW Group AG
+ * Copyright (c) 2021,2022 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities;
 using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities.Entities;
@@ -11,13 +28,11 @@ namespace  Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Seeder;
 
 public class AddressSeeder : ICustomSeeder
 {
-    private readonly IHostEnvironment _environment;
     private readonly PortalDbContext _context;
     private readonly ILogger<AddressSeeder> _logger;
 
-    public AddressSeeder(IHostEnvironment environment, PortalDbContext context, ILogger<AddressSeeder> logger)
+    public AddressSeeder(PortalDbContext context, ILogger<AddressSeeder> logger)
     {
-        _environment = environment;
         _context = context;
         _logger = logger;
     }
