8202233a8eba285d80e5dafceaf75589e652e403
Phil Schneider <info@philschneider.de>
2022-12-12 12:37:38 +0100

    CPLP-1840 add unit test

--- tests/portalbackend/PortalBackend.DBAccess.Tests/UserRepositoryTests.cs
+++ tests/portalbackend/PortalBackend.DBAccess.Tests/UserRepositoryTests.cs
@@ -323,6 +323,46 @@ public class UserRepositoryTests : IAssemblyFixture<TestDbFixture>
 
     #endregion
 
+    #region GetCompanyUserWithRoleIdForCompany
+
+    [Fact]
+    public async Task GetCompanyUserWithRoleIdForCompany_WithExistingUserForRole_ReturnsExpectedUserId()
+    {
+        // Arrange
+        var (sut, _) = await CreateSut().ConfigureAwait(false);
+        
+        // Act
+        var result = await sut
+            .GetCompanyUserWithRoleIdForCompany(new[] {new Guid("607818be-4978-41f4-bf63-fa8d2de51154")}, new Guid("2dc4249f-b5ca-4d42-bef1-7a7a950a4f87"))
+            .ToListAsync().ConfigureAwait(false);
+
+        // Assert
+        result.Should().HaveCount(1);
+        result.First().Should().Be(new Guid("ac1cf001-7fbc-1f2f-817f-bce058019993"));
+    }
+
+    #endregion
+    
+    #region GetCompanyUserWithRoleIdForCompany
+
+    [Fact]
+    public async Task GetCompanyUserEmailForCompanyAndRoleId_WithExistingUserForRole_ReturnsExpectedEmail()
+    {
+        // Arrange
+        var (sut, _) = await CreateSut().ConfigureAwait(false);
+        
+        // Act
+        var result = await sut
+            .GetCompanyUserEmailForCompanyAndRoleId(new[] {new Guid("607818be-4978-41f4-bf63-fa8d2de51154")}, new Guid("2dc4249f-b5ca-4d42-bef1-7a7a950a4f87"))
+            .ToListAsync().ConfigureAwait(false);
+        
+        // Assert
+        result.Should().HaveCount(1);
+        result.First().Should().Be("tester.user4@test.de");
+    }
+
+    #endregion
+
     private async Task<(UserRepository, PortalDbContext)> CreateSut()
     {
         var context = await _dbTestDbFixture.GetPortalDbContext().ConfigureAwait(false);
