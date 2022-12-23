62429612b7c8508cc03d6a0d41822b693090fc53
qxz2mqe <md-majid.akhter@partner.bmw.in>
2022-12-23 10:57:08 +0530

    CPLP-1927-Changes for single documentid

--- tests/marketplace/Apps.Service.Tests/BusinessLogic/AppBusinessLogicTests.cs
+++ tests/marketplace/Apps.Service.Tests/BusinessLogic/AppBusinessLogicTests.cs
@@ -118,7 +118,7 @@ public class AppBusinessLogicTests
     public async Task GetAllActiveAppsAsync_ExecutesSuccessfully()
     {
         // Arrange
-        var results = _fixture.CreateMany<ValueTuple<Guid, string?, string, IEnumerable<string>, IEnumerable<Guid>, string?, string?>>(5);
+        var results = _fixture.CreateMany<ValueTuple<Guid, string?, string, IEnumerable<string>, Guid, string?, string?>>(5);
         A.CallTo(() => _offerRepository.GetAllActiveAppsAsync(A<string>._)).Returns(results.ToAsyncEnumerable());
 
         var sut = new AppsBusinessLogic(_portalRepositories, null!, null!, A.Fake<IOptions<AppsSettings>>(), A.Fake<MailingService>());
