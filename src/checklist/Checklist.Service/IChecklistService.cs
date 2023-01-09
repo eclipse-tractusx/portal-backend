30f57bfda4d375e1829a48c4cb0cc4e25a2d8e34
Phil Schneider <info@philschneider.de>
2023-01-09 14:18:35 +0100

    CPLP-1963 move update company bpn to checklist service

--- src/checklist/Checklist.Service/IChecklistService.cs
+++ src/checklist/Checklist.Service/IChecklistService.cs
@@ -1,15 +1,19 @@
-using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities.Enums;
-
 namespace Org.Eclipse.TractusX.Portal.Backend.Checklist.Service;
 
 public interface IChecklistService
 {
     /// <summary>
-    /// Updates the status of the checklist entry for the given id
+    /// Triggers the bpn data push
     /// </summary>
-    /// <param name="applicationId">ID of the checklist entry</param>
-    /// <param name="statusId">Id of the new status</param>
-    Task UpdateBpnStatusAsync(Guid applicationId, ChecklistEntryStatusId statusId);
-
+    /// <param name="applicationId">Id of the application</param>
+    /// <param name="iamUserId">the current user</param>
+    /// <param name="cancellationToken">Cancellation Token</param>
     Task TriggerBpnDataPush(Guid applicationId, string iamUserId, CancellationToken cancellationToken);
+    
+    /// <summary>
+    /// Updates the company bpn
+    /// </summary>
+    /// <param name="applicationId">Id of the application to update the bpn</param>
+    /// <param name="bpn">the bpn to set</param>
+    Task UpdateCompanyBpn(Guid applicationId, string bpn);
 }
