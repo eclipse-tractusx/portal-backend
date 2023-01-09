188a41402452fd519769a8a59d5f2a95b28ac244
Phil Schneider <info@philschneider.de>
2023-01-09 12:32:27 +0100

    CPLP-1963 include failed requests as well

--- tests/checklist/Checklist.Service.Tests/ChecklistServiceTests.cs
+++ tests/checklist/Checklist.Service.Tests/ChecklistServiceTests.cs
@@ -35,6 +35,7 @@ public class ChecklistServiceTests
 {
     private static readonly Guid ApplicationWithoutBpnId = new ("0a9bd7b1-e692-483e-8128-dbf52759c7a5");
     private static readonly Guid ApplicationWithBpnId = new ("c244f79a-7faf-4c59-bb85-fbfdf72ce46f");
+    private static readonly Guid NotExistingApplicationId = new ("1942e8d3-b545-4fbc-842c-01a694f84390");
     private static readonly string IamUserId = new Guid("4C1A6851-D4E7-4E10-A011-3732CD045E8A").ToString();
     private readonly IFixture _fixture;
     private readonly IPortalRepositories _portalRepositories;
@@ -111,7 +112,7 @@ public class ChecklistServiceTests
     public async Task UpdateBpnStatus_WithStatus_SetsExpectedStatus(ChecklistEntryStatusId statusId)
     {
         // Arrange
-        var entry = new ApplicationChecklistEntry(ApplicationWithBpnId, ChecklistEntryTypeId.BUSINESS_PARTNER_NUMBER, ChecklistEntryStatusId.TO_DO, DateTimeOffset.UtcNow);
+        var entry = new ApplicationChecklistEntry(ApplicationWithoutBpnId, ChecklistEntryTypeId.BUSINESS_PARTNER_NUMBER, ChecklistEntryStatusId.TO_DO, DateTimeOffset.UtcNow);
         SetupFakesForUpdate(entry);
         
         // Act
@@ -127,19 +128,55 @@ public class ChecklistServiceTests
     #region TriggerBpnDataPush
     
     [Fact]
-    public async Task TriggerBpnDataPush_WithNotExistingApplication_ThrowsNotFoundException()
+    public async Task TriggerBpnDataPush_WithValidData_CallsExpected()
     {
         // Arrange
-        var notExistingApplicationId = Guid.NewGuid();
-        A.CallTo(() => _companyRepository.GetBpdmDataForApplicationAsync(IamUserId, notExistingApplicationId))
-            .ReturnsLazily(() => (BpdmData?)null);
+        var entry = new ApplicationChecklistEntry(ApplicationWithoutBpnId, ChecklistEntryTypeId.BUSINESS_PARTNER_NUMBER, ChecklistEntryStatusId.TO_DO, DateTimeOffset.UtcNow);
+        SetupFakesForTrigger(entry);
+
+        // Act
+        await _service.TriggerBpnDataPush(ApplicationWithoutBpnId, IamUserId, CancellationToken.None).ConfigureAwait(false);
+
+        // Assert
+        A.CallTo(() => _bpdmService.TriggerBpnDataPush(A<BpdmTransferData>._, A<CancellationToken>._)).MustHaveHappenedOnceExactly();
+        A.CallTo(() => _applicationChecklistRepository.AttachAndModifyApplicationChecklist(ApplicationWithoutBpnId, ChecklistEntryTypeId.BUSINESS_PARTNER_NUMBER, A<Action<ApplicationChecklistEntry>>._)).MustHaveHappenedOnceExactly();
+        A.CallTo(() => _portalRepositories.SaveAsync()).MustHaveHappenedOnceExactly();
+        entry.StatusId.Should().Be(ChecklistEntryStatusId.IN_PROGRESS);
+    }
+
+    [Fact]
+    public async Task TriggerBpnDataPush_WithValidDataAndFailingBpdmServiceCall_ThrowsExceptionAndDoesntUpdateEntry()
+    {
+        // Arrange
+        var entry = new ApplicationChecklistEntry(ApplicationWithoutBpnId, ChecklistEntryTypeId.BUSINESS_PARTNER_NUMBER, ChecklistEntryStatusId.TO_DO, DateTimeOffset.UtcNow);
+        A.CallTo(() => _bpdmService.TriggerBpnDataPush(A<BpdmTransferData>._, CancellationToken.None))
+            .Throws(new ServiceException("Bpdm Service Call failed."));
+        
+        SetupFakesForTrigger(entry);
 
         // Act
         async Task Act() => await _service.TriggerBpnDataPush(ApplicationWithoutBpnId, IamUserId, CancellationToken.None).ConfigureAwait(false);
 
         // Assert
+        await Assert.ThrowsAsync<ServiceException>(Act);
+        A.CallTo(() => _bpdmService.TriggerBpnDataPush(A<BpdmTransferData>._, A<CancellationToken>._)).MustHaveHappenedOnceExactly();
+        A.CallTo(() => _applicationChecklistRepository.AttachAndModifyApplicationChecklist(ApplicationWithoutBpnId, ChecklistEntryTypeId.BUSINESS_PARTNER_NUMBER, A<Action<ApplicationChecklistEntry>>._)).MustNotHaveHappened();
+        A.CallTo(() => _portalRepositories.SaveAsync()).MustNotHaveHappened();
+        entry.StatusId.Should().Be(ChecklistEntryStatusId.TO_DO);
+    }
+
+    [Fact]
+    public async Task TriggerBpnDataPush_WithNotExistingApplication_ThrowsNotFoundException()
+    {
+        // Arrange
+        SetupFakesForTrigger();
+
+        // Act
+        async Task Act() => await _service.TriggerBpnDataPush(NotExistingApplicationId, IamUserId, CancellationToken.None).ConfigureAwait(false);
+
+        // Assert
         var ex = await Assert.ThrowsAsync<NotFoundException>(Act);
-        ex.Message.Should().Be($"Application {notExistingApplicationId} does not exists.");
+        ex.Message.Should().Be($"Application {NotExistingApplicationId} does not exists.");
         A.CallTo(() => _bpdmService.TriggerBpnDataPush(A<BpdmTransferData>._, A<CancellationToken>._)).MustNotHaveHappened();
     }
 
@@ -150,9 +187,10 @@ public class ChecklistServiceTests
         var createdApplicationId = Guid.NewGuid();
         A.CallTo(() => _companyRepository.GetBpdmDataForApplicationAsync(IamUserId, createdApplicationId))
             .ReturnsLazily(() => new BpdmData(CompanyApplicationStatusId.CREATED, null!, null!, null!, null!, null!, true));
+        SetupFakesForTrigger();
 
         // Act
-        async Task Act() => await _service.TriggerBpnDataPush(ApplicationWithoutBpnId, IamUserId, CancellationToken.None).ConfigureAwait(false);
+        async Task Act() => await _service.TriggerBpnDataPush(createdApplicationId, IamUserId, CancellationToken.None).ConfigureAwait(false);
 
         // Assert
         var ex = await Assert.ThrowsAsync<ArgumentException>(Act);
@@ -164,13 +202,10 @@ public class ChecklistServiceTests
     public async Task TriggerBpnDataPush_WithNotExistingUser_ThrowsArgumentException()
     {
         // Arrange
-        var applicationId = Guid.NewGuid();
-        var wrongUserId = Guid.NewGuid().ToString();
-        A.CallTo(() => _companyRepository.GetBpdmDataForApplicationAsync(wrongUserId, applicationId))
-            .ReturnsLazily(() => new BpdmData(CompanyApplicationStatusId.SUBMITTED, null!, null!, null!, null!, null!, false));
+        SetupFakesForTrigger();
 
         // Act
-        async Task Act() => await _service.TriggerBpnDataPush(applicationId, wrongUserId, CancellationToken.None).ConfigureAwait(false);
+        async Task Act() => await _service.TriggerBpnDataPush(ApplicationWithoutBpnId, Guid.NewGuid().ToString(), CancellationToken.None).ConfigureAwait(false);
 
         // Assert
         var ex = await Assert.ThrowsAsync<ControllerArgumentException>(Act);
@@ -182,21 +217,37 @@ public class ChecklistServiceTests
     public async Task TriggerBpnDataPush_WithEmptyZipCode_ThrowsConflictException()
     {
         // Arrange
-        var createdApplicationId = _fixture.Create<Guid>();
+        var applicationWithoutZipId = _fixture.Create<Guid>();
         var data = _fixture.Build<BpdmData>()
             .With(x => x.ApplicationStatusId, CompanyApplicationStatusId.SUBMITTED)
             .With(x => x.IsUserInCompany, true)
             .With(x => x.ZipCode, (string?)null)
             .Create();
-        A.CallTo(() => _companyRepository.GetBpdmDataForApplicationAsync(IamUserId, createdApplicationId))
+        A.CallTo(() => _companyRepository.GetBpdmDataForApplicationAsync(IamUserId, applicationWithoutZipId))
             .ReturnsLazily(() => data);
+        SetupFakesForTrigger();
 
         // Act
-        async Task Act() => await _service.TriggerBpnDataPush(createdApplicationId, IamUserId, CancellationToken.None).ConfigureAwait(false);
+        async Task Act() => await _service.TriggerBpnDataPush(applicationWithoutZipId, IamUserId, CancellationToken.None).ConfigureAwait(false);
 
         // Assert
         var ex = await Assert.ThrowsAsync<ConflictException>(Act);
         ex.Message.Should().Be("ZipCode must not be empty");
+        A.CallTo(() => _bpdmService.TriggerBpnDataPush(A<BpdmTransferData>._, A<CancellationToken>._)).MustNotHaveHappened();
+    }
+
+    [Fact]
+    public async Task TriggerBpnDataPush_WithChecklistStatusAlreadyDone_ThrowsConflictException()
+    {
+        // Arrange
+        SetupFakesForTrigger();
+
+        // Act
+        async Task Act() => await _service.TriggerBpnDataPush(ApplicationWithBpnId, IamUserId, CancellationToken.None).ConfigureAwait(false);
+
+        // Assert
+        var ex = await Assert.ThrowsAsync<ConflictException>(Act);
+        ex.Message.Should().Be($"{ChecklistEntryTypeId.BUSINESS_PARTNER_NUMBER} is not available as next step");
         A.CallTo(() => _bpdmService.TriggerBpnDataPush(A<BpdmTransferData>._, A<CancellationToken>._)).MustNotHaveHappened();
     }
 
@@ -222,6 +273,39 @@ public class ChecklistServiceTests
                     setFields.Invoke(applicationChecklistEntry);
                 });
 
+        A.CallTo(() => _portalRepositories.GetInstance<IApplicationChecklistRepository>()).Returns(_applicationChecklistRepository);
+    }
+
+    private void SetupFakesForTrigger(ApplicationChecklistEntry? applicationChecklistEntry = null)
+    {
+        if (applicationChecklistEntry != null)
+        {
+            SetupFakesForUpdate(applicationChecklistEntry);
+        }
+
+        var validData = _fixture.Build<BpdmData>()
+            .With(x => x.ApplicationStatusId, CompanyApplicationStatusId.SUBMITTED)
+            .With(x => x.IsUserInCompany, true)
+            .With(x => x.ZipCode, "50668")
+            .Create();
+        A.CallTo(() => _companyRepository.GetBpdmDataForApplicationAsync(IamUserId, NotExistingApplicationId))
+            .ReturnsLazily(() => (BpdmData?)null);
+
+        A.CallTo(() => _companyRepository.GetBpdmDataForApplicationAsync(A<string>.That.Not.Matches(x => x == IamUserId), ApplicationWithoutBpnId))
+            .ReturnsLazily(() => new BpdmData(CompanyApplicationStatusId.SUBMITTED, null!, null!, null!, null!, null!, false));
+        A.CallTo(() => _companyRepository.GetBpdmDataForApplicationAsync(IamUserId, A<Guid>.That.Matches(x => x == ApplicationWithoutBpnId || x == ApplicationWithBpnId)))
+            .ReturnsLazily(() => validData);
+
+        A.CallTo(() => _applicationChecklistRepository.GetChecklistDataAsync(ApplicationWithoutBpnId))
+            .ReturnsLazily(() => new Dictionary<ChecklistEntryTypeId, ChecklistEntryStatusId>
+            {
+                {ChecklistEntryTypeId.BUSINESS_PARTNER_NUMBER, ChecklistEntryStatusId.TO_DO},
+            });
+
+        A.CallTo(() => _applicationChecklistRepository.GetChecklistDataAsync(ApplicationWithBpnId))
+            .ReturnsLazily(() => new Dictionary<ChecklistEntryTypeId, ChecklistEntryStatusId>());
+
+        A.CallTo(() => _portalRepositories.GetInstance<ICompanyRepository>()).Returns(_companyRepository);
         A.CallTo(() => _portalRepositories.GetInstance<IApplicationChecklistRepository>()).Returns(_applicationChecklistRepository);
     }
 
