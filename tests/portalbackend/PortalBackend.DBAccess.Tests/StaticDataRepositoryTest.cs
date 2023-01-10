1c20878c455d7a975e7d7dbe236a7f6a4cf6867d
qxz2mqe <md-majid.akhter@partner.bmw.in>
2023-01-10 12:42:37 +0530

    CPLP-1902-Unit Test

---
+++ tests/portalbackend/PortalBackend.DBAccess.Tests/StaticDataRepositoryTest.cs
@@ -0,0 +1,67 @@
+/********************************************************************************
+ * Copyright (c) 2021,2022 BMW Group AG
+ * Copyright (c) 2021,2022 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+using AutoFixture;
+using AutoFixture.AutoFakeItEasy;
+using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.DBAccess.Repositories;
+using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.DBAccess.Tests.Setup;
+using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities;
+using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities.Enums;
+using FluentAssertions;
+using Xunit;
+using Xunit.Extensions.AssemblyFixture;
+
+namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.DBAccess.Tests;
+
+public class StaticDataRepositoryTest : IAssemblyFixture<TestDbFixture>
+{
+     private readonly TestDbFixture _dbTestDbFixture;
+
+    public StaticDataRepositoryTest(TestDbFixture testDbFixture)
+    {
+        var fixture = new Fixture().Customize(new AutoFakeItEasyCustomization { ConfigureMembers = true });
+        fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+            .ForEach(b => fixture.Behaviors.Remove(b));
+
+        fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        _dbTestDbFixture = testDbFixture;
+    }
+    
+    [Fact]
+    public async Task GetCompanyIdentifiers_ReturnsExpectedResult()
+    {
+        // Arrange
+        var sut = await CreateSut().ConfigureAwait(false);
+
+        // Act
+        var result = await sut.GetCompanyIdentifiers("DE").ToListAsync().ConfigureAwait(false);
+
+        // Assert
+        result.Select(x=>x.IdentifierData).Select(x=>x.Id).FirstOrDefault().Should().Be((int)UniqueIdentifierId.COMMERCIAL_REG_NUMBER);
+    }
+
+    private async Task<StaticDataRepository> CreateSut()
+    {
+        var context = await _dbTestDbFixture.GetPortalDbContext().ConfigureAwait(false);
+        var sut = new StaticDataRepository(context);
+        return sut;
+    }
+
+}
