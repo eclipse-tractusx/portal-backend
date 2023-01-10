3a390e74d06c53d2dee6ae3b4d241b90ad8d7d38
Phil Schneider <info@philschneider.de>
2023-01-10 08:39:05 +0100

    CPLP-1676 recreate migration

--- src/portalbackend/PortalBackend.Migrations/Migrations/20230110073548_CPLP-1676-AddServiceAccountType.cs
+++ src/portalbackend/PortalBackend.Migrations/Migrations/20230110073548_CPLP-1676-AddServiceAccountType.cs
@@ -1,24 +1,4 @@
-﻿/********************************************************************************
- * Copyright (c) 2021,2022 BMW Group AG
- * Copyright (c) 2021,2022 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License, Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- ********************************************************************************/
-
-using System;
+﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -52,7 +32,7 @@ namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migration
                 table: "company_service_accounts",
                 type: "integer",
                 nullable: false,
-                defaultValue: 0);
+                defaultValue: 1);
 
             migrationBuilder.AddColumn<Guid>(
                 name: "offer_subscription_id",
