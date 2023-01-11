6cb026ea760c5411c1a4b9c2262ae8d8d81e7034
Norbert Truchsess <norbert.truchsess@t-online.de>
2023-01-11 23:30:43 +0100

    CPLP-1920 adjust db unit test

--- tests/shared/Tests.Shared/TestSeeds/BaseSeed.cs
+++ tests/shared/Tests.Shared/TestSeeds/BaseSeed.cs
@@ -162,6 +162,10 @@ public static class BaseSeed
         {
             new (new Guid("99C5FD12-8085-4DE2-ABFD-215E1EE4BAA4"), new Guid("6ca00fc6-4c82-47d8-8616-059ebe65232b"))
         });
+        dbContext.OfferDescriptions.AddRange(new OfferDescription[] {
+            new (new Guid("99C5FD12-8085-4DE2-ABFD-215E1EE4BAA5"), "en", "some long Description", "some short Description"),
+            new (new Guid("99C5FD12-8085-4DE2-ABFD-215E1EE4BAA5"), "de", "eine lange Beschreibung", "eine kurze Beschreibung"),
+        });
         dbContext.AppInstances.AddRange(AppInstanceData.AppInstances);
         dbContext.OfferSubscriptions.AddRange(new List<OfferSubscription>
         {
