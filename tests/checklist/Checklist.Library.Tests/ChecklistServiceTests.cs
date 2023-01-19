90c4c6d5c15bc3d5febc4ccdb2dd6d9d433e2662
Norbert Truchsess <norbert.truchsess@t-online.de>
2023-01-19 16:23:40 +0100

    CPLP-1965 fix unit-test

--- tests/checklist/Checklist.Library.Tests/ChecklistServiceTests.cs
+++ tests/checklist/Checklist.Library.Tests/ChecklistServiceTests.cs
@@ -331,17 +331,17 @@ public class ChecklistServiceTests
             SetupForUpdate(applicationChecklistEntry);
         }
 
-        A.CallTo(() => _applicationRepository.GetCompanyAndApplicationDetailsForSubmittedApplicationAsync(IdWithoutBpn))
-            .ReturnsLazily(() => new ValueTuple<Guid, string, string?, string>(Guid.NewGuid(), ValidCompanyName, null, "DE"));
-        A.CallTo(() => _applicationRepository.GetCompanyAndApplicationDetailsForSubmittedApplicationAsync(IdWithBpn))
-            .ReturnsLazily(() => new ValueTuple<Guid, string, string?, string>(CompanyId, ValidCompanyName, ValidBpn, "DE"));
-        A.CallTo(() => _applicationRepository.GetCompanyAndApplicationDetailsForSubmittedApplicationAsync(IdWithFailingCustodian))
-            .ReturnsLazily(() => new ValueTuple<Guid, string, string?, string>(Guid.NewGuid(), "a company", "bpnNotValidForWallet", "DE"));
-        A.CallTo(() => _applicationRepository.GetCompanyAndApplicationDetailsForSubmittedApplicationAsync(IdWithCustodianUnavailable))
-            .ReturnsLazily(() => new ValueTuple<Guid, string, string?, string>(Guid.NewGuid(), "a company", "custodiaNotAvailable", "DE"));
-        
-        A.CallTo(() => _applicationRepository.GetCompanyAndApplicationDetailsForSubmittedApplicationAsync(A<Guid>.That.Not.Matches(x => x == IdWithBpn || x == IdWithoutBpn || x == IdWithFailingCustodian || x == IdWithCustodianUnavailable)))
-            .ReturnsLazily(() => new ValueTuple<Guid, string, string?, string>());
+        A.CallTo(() => _applicationRepository.GetCompanyAndApplicationDetailsForCreateWalletAsync(IdWithoutBpn))
+            .Returns(new ValueTuple<Guid, string, string?>(Guid.NewGuid(), ValidCompanyName, null));
+        A.CallTo(() => _applicationRepository.GetCompanyAndApplicationDetailsForCreateWalletAsync(IdWithBpn))
+            .Returns(new ValueTuple<Guid, string, string?>(CompanyId, ValidCompanyName, ValidBpn));
+        A.CallTo(() => _applicationRepository.GetCompanyAndApplicationDetailsForCreateWalletAsync(IdWithFailingCustodian))
+            .Returns(new ValueTuple<Guid, string, string?>(Guid.NewGuid(), "a company", "bpnNotValidForWallet"));
+        A.CallTo(() => _applicationRepository.GetCompanyAndApplicationDetailsForCreateWalletAsync(IdWithCustodianUnavailable))
+            .Returns(new ValueTuple<Guid, string, string?>(Guid.NewGuid(), "a company", "custodiaNotAvailable"));
+
+        A.CallTo(() => _applicationRepository.GetCompanyAndApplicationDetailsForCreateWalletAsync(A<Guid>.That.Not.Matches(x => x == IdWithBpn || x == IdWithoutBpn || x == IdWithFailingCustodian || x == IdWithCustodianUnavailable)))
+            .Returns(((Guid, string, string))default);
 
         A.CallTo(() => _custodianBusinessLogic.CreateWalletAsync(IdWithBpn, CancellationToken.None))
             .ReturnsLazily(() => "It worked.");
