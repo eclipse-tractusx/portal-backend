0471fcace242eb1d8bc13dd7503ee23e1871bf45
Phil Schneider <info@philschneider.de>
2023-03-24 14:04:23 +0100

    revert: unique index for consent (#283)
    Refs: CPLP-2363

--- src/portalbackend/PortalBackend.PortalEntities/PortalDbContext.cs
+++ src/portalbackend/PortalBackend.PortalEntities/PortalDbContext.cs
@@ -755,9 +755,6 @@ public class PortalDbContext : DbContext
                 .WithMany(p => p!.Consents)
                 .HasForeignKey(d => d.ConsentStatusId)
                 .OnDelete(DeleteBehavior.ClientSetNull);
-
-            entity.HasIndex(x => new {x.AgreementId, x.CompanyId})
-                .IsUnique();
         });
         
         modelBuilder.Entity<ConsentAssignedOfferSubscription>(entity =>
