b96544cbc76d1412f227935e492466c5af907732
irinamesh <irina.meshcheryakova@tngtech.com>
2023-04-28 18:09:02 +0200

    CPLP-2596: add tests for get requests in Administration.Service

---
+++ tests/administration/Administration.Service.Tests/RestAssured/AdministrationEndpointTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Org.Eclipse.TractusX.Portal.Backend.Tests.Shared;
+using static RestAssured.Dsl;
+
+namespace Org.Eclipse.TractusX.Portal.Backend.Administration.Service.Tests.RestAssured;
+
+public class AdministrationEndpointTests
+{
+    private readonly string _baseUrl = "https://portal-backend.dev.demo.catena-x.net";
+    private readonly string _endPoint = "/api/administration";
+    private readonly string _operatorToken;
+    private static string _applicationId;
+    private static string _companyName = "Catena-X";
+    
+    public AdministrationEndpointTests()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<Secrets>()
+            .Build();
+        _operatorToken = configuration.GetValue<string>("Secrets:OperatorToken");
+        _applicationId = new (configuration.GetValue<string>("Secrets:ApplicationId"));
+    }
+    
+    [Fact]
+    public void GetApplicationDetails_ReturnsExpectedResult()
+    {
+        // Given
+        Given()
+            .RelaxedHttpsValidation()
+            .Header(
+                "authorization",
+                $"Bearer {_operatorToken}")
+            .When()
+            .Get($"{_baseUrl}{_endPoint}/registration/applications?companyName={_companyName}/?page=0&size=10&sorting=DateDesc")
+            .Then()
+            .StatusCode(200);
+    }
+    
+    [Fact]
+    public void GetCompanyWithAddress_ReturnsExpectedResult()
+    {
+        // Given
+        Given()
+            .RelaxedHttpsValidation()
+            .Header(
+                "authorization",
+                $"Bearer {_operatorToken}")
+            .When()
+            .Get($"{_baseUrl}{_endPoint}/registration/application/{_applicationId}/companyDetailsWithAddress")
+            .Then()
+            .StatusCode(200);
+    }
+}
--- tests/shared/Tests.Shared/RestAssured/Secrets.cs
+++ tests/shared/Tests.Shared/RestAssured/Secrets.cs
@@ -2,6 +2,7 @@ namespace Org.Eclipse.TractusX.Portal.Backend.Tests.Shared;
 
 public class Secrets
 {
+    public string OperatorToken { get; set; }
     public string UserToken { get; set; }
     public string CompanyToken { get; set; }
     public string CompanyUserId { get; set; }
