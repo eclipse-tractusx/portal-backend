70dcaca5d5e3b4d54778edf1c87f7a34e203a46b
irinamesh <irina.meshcheryakova@tngtech.com>
2023-05-11 11:15:46 +0200

    CPLP-2513: add fetch password

--- tests/registration/Registration.Service.Tests/RestAssured/TempMailApiRequests.cs
+++ tests/registration/Registration.Service.Tests/RestAssured/TempMailApiRequests.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Schema;
 using Org.Eclipse.TractusX.Portal.Backend.Registration.Service.Model;
 using Org.Eclipse.TractusX.Portal.Backend.Tests.Shared;
+using PasswordGenerator;
 using Xunit;
 using static RestAssured.Dsl;
 namespace Registration.Service.Tests.RestAssured;
@@ -20,6 +23,39 @@ public class TempMailApiRequests
         _tempMailApiKey = configuration.GetValue<string>("Secrets:TempMailApiKey");
     }
 
+    [Fact]
+    public string? FetchPassword()
+    {
+        TempMailMessageData? passwordMessage = GetPasswordMessage();
+
+        if (passwordMessage != null)
+        {
+            //search for password in the first span of the body
+            Regex r = new Regex(@"<span[^>].*?>([^<]*)<\/span>", RegexOptions.IgnoreCase);
+            string password = r.Matches(passwordMessage.mail_html).First().Groups[1].Value.Trim('\n').Trim();
+            return password;
+        }
+        
+        return null;
+    }
+
+    private TempMailMessageData? GetPasswordMessage()
+    {
+        var hashedEmailAddress = CreateMd5();
+        var messages = (List<TempMailMessageData>)Given()
+            .RelaxedHttpsValidation()
+            .Header(
+                "apikey",
+                $"{_tempMailApiKey}")
+            .When()
+            .Get($"{_baseUrl}{_endPoint}/mail/id/{hashedEmailAddress}")
+            .Then()
+            .StatusCode(200)
+            .Extract().As(typeof(List<TempMailMessageData>));
+        var passwordMessage = messages.First(item => item.mail_subject.Contains("Password required"));
+        return passwordMessage;
+    }
+    
     private string GetDomain()
     {
         var data = (string[])Given()
@@ -33,33 +69,13 @@ public class TempMailApiRequests
             .StatusCode(200)
             .Extract()
             .As(typeof(string[]));
-        return data[0];
-    }
-
-
-    [Fact]
-    public void CheckMailbox()
-    {
-        var hashedEmailAddress = CreateMd5();
-        var data = Given()
-            .RelaxedHttpsValidation()
-            .Header(
-                "apikey",
-                $"{_tempMailApiKey}")
-            .When()
-            .Get($"{_baseUrl}{_endPoint}/mail/id/{hashedEmailAddress}")
-            .Then()
-            .StatusCode(200)
-            .Extract().As(typeof(List<TempMailMessageData>));
-        //.As(typeof(string[]));
-        //return data[0];
+        return data[1];
     }
 
     //https://stackoverflow.com/questions/11454004/calculate-a-md5-hash-from-a-string
     private string CreateMd5()
     {
         var emailAddress = _apiTestUsername + GetDomain();
-        // Use input string to calculate MD5 hash
         using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
         {
             byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(emailAddress);
@@ -71,7 +87,7 @@ public class TempMailApiRequests
 
     //TODO: Delete all messages in mailbox after getting password
     /*
-    private void DeleteMessagesInMailbox()
+    private void DeleteMessages()
     {
         foreach (var mailId in mailIds)
         {
