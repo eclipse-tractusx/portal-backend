b69279456bdd402a3ad36b2c6dfa8b1abf5ce502
irinamesh <irina.meshcheryakova@tngtech.com>
2023-05-16 14:22:31 +0200

    CPLP-2596: refactoring - move 1secmail functions to separate class

---
+++ tests/registration/Registration.Service.Tests/RestAssured/OneSecMailApiRequests.cs
@@ -0,0 +1,56 @@
+﻿namespace Registration.Service.Tests.RestAssured;
+
+public class OneSecMailApiRequests
+{
+    // private string GenerateRandomEmailAddress()
+    // {
+    //     // Given
+    //     var emailAddress = (string[])Given()
+    //         .RelaxedHttpsValidation()
+    //         .When()
+    //         .Get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1")
+    //         .Then()
+    //         .And()
+    //         .StatusCode(200)
+    //         .Extract().As(typeof(string[]));
+    //     _userEmailAddress = emailAddress[0].Split("@");
+    //     return emailAddress[0];
+    // }
+    //
+    // private MailboxData[] CheckMailBox()
+    // {
+    //     // Given
+    //     var emails = (MailboxData[])Given()
+    //         .RelaxedHttpsValidation()
+    //         .When()
+    //         .Get(
+    //             $"https://www.1secmail.com/api/v1/?action=getMessages&login={_userEmailAddress[0]}&domain={_userEmailAddress[1]}")
+    //         .Then()
+    //         .And()
+    //         .StatusCode(200)
+    //         .Extract().As(typeof(MailboxData[]));
+    //     return emails;
+    // }
+    //
+    // private EmailMessageData? FetchPassword()
+    // {
+    //     // Given
+    //     var emails = CheckMailBox();
+    //     if (emails.Length != 0)
+    //     {
+    //         var passwordMail = emails[0]?.Id;
+    //         var messageData = (EmailMessageData)Given()
+    //             .RelaxedHttpsValidation()
+    //             .When()
+    //             .Get(
+    //                 $"https://www.1secmail.com/api/v1/?action=readMessage&login={_userEmailAddress[0]}&domain={_userEmailAddress[1]}&id={passwordMail}")
+    //             .Then()
+    //             .And()
+    //             .StatusCode(200)
+    //             .Extract().As(typeof(EmailMessageData));
+    //         return messageData;
+    //     }
+    //
+    //     return null;
+    // }
+}
