16c7a2e7955484e9a2836233133c0b09cda54f6c
qxz2mqe <md-majid.akhter@partner.bmw.in>
2023-05-19 12:37:23 +0530

    feat!:added comments

--- src/marketplace/Apps.Service/ViewModels/AppDetailResponse.cs
+++ src/marketplace/Apps.Service/ViewModels/AppDetailResponse.cs
@@ -45,7 +45,7 @@ namespace Org.Eclipse.TractusX.Portal.Backend.Apps.Service.ViewModels;
 /// <param name="Documents">documents assigned to offer</param>
 /// <param name="PrivacyPolicies">Privacy Policies assigned to offer</param>
 /// <param name="IsSingleInstance">Defines whether the app is a single instance</param>
-/// <param name="TechnicalUserProfile">Technical User Profile</param>
+/// <param name="TechnicalUserProfile">Technical User Profile for app</param>
 public record AppDetailResponse(
     Guid Id,
     string Title,
