5c869707dfb02e404a92255b6dceb530c53a6253
Phil Schneider <info@philschneider.de>
2023-06-01 10:00:20 +0200

    chore: fix formatting
    Refs: CPLP-2344

--- src/portalbackend/PortalBackend.Migrations/Migrations/20230601075130_1.5.0-rc1.cs
+++ src/portalbackend/PortalBackend.Migrations/Migrations/20230601075130_1.5.0-rc1.cs
@@ -1,4 +1,4 @@
-﻿/********************************************************************************
+/********************************************************************************
  * Copyright (c) 2021, 2023 BMW Group AG
  * Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
  *
@@ -18,8 +18,8 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-using System;
 using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 #nullable disable
 
