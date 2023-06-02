ec70d330499646bb3cc2b8d17e7acaab90f80438
Phil Schneider <info@philschneider.de>
2023-06-02 17:17:27 +0200

    chore: recreate migration
    Refs: CPLP-2553

--- src/portalbackend/PortalBackend.Migrations/Migrations/20230602151136_CPLP-2553-AlignUsers.cs
+++ src/portalbackend/PortalBackend.Migrations/Migrations/20230602151136_CPLP-2553-AlignUsers.cs
@@ -1,25 +1,5 @@
-/********************************************************************************
- * Copyright (c) 2021, 2023 BMW Group AG
- * Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License, Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- ********************************************************************************/
-
+﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
-using System;
 
 #nullable disable
 
@@ -433,7 +413,7 @@ namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migration
                 table: "company_users",
                 type: "uuid",
                 nullable: false,
-                defaultValue: Guid.Empty);
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
 
             migrationBuilder.AddColumn<int>(
                 name: "company_user_status_id",
@@ -473,7 +453,7 @@ namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migration
                 table: "company_service_accounts",
                 type: "uuid",
                 nullable: false,
-                defaultValue: Guid.Empty);
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
 
             migrationBuilder.CreateTable(
                 name: "company_service_account_assigned_roles",
