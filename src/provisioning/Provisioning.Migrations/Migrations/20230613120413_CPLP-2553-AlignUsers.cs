9350df5754ab268d6c399efe9c626a03d6b5497f
Norbert Truchsess <norbert.truchsess@t-online.de>
2023-06-13 14:48:24 +0200

    chore(provisioning) fix formating

--- src/provisioning/Provisioning.Migrations/Migrations/20230613120413_CPLP-2553-AlignUsers.cs
+++ src/provisioning/Provisioning.Migrations/Migrations/20230613120413_CPLP-2553-AlignUsers.cs
@@ -18,7 +18,6 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
