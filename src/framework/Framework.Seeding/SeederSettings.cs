06c416458c1c473bbd6fc44c0b48102e1a594349
Phil Schneider <info@philschneider.de>
2023-06-29 10:33:34 +0200

    chore: add required attribute to settings
    Refs: CPLP-2788

--- src/framework/Framework.Seeding/SeederSettings.cs
+++ src/framework/Framework.Seeding/SeederSettings.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Org.Eclipse.TractusX.Portal.Backend.Framework.Seeding;
 
 public class SeederSettings
@@ -11,10 +13,12 @@ public class SeederSettings
     /// <summary>
     /// Configurable paths where to check for the seeding data
     /// </summary>
+    [Required]
     public IEnumerable<string> DataPaths { get; set; }
 
     /// <summary>
     /// Configurable environments for the testdata
     /// </summary>
+    [Required]
     public IEnumerable<string> TestDataEnvironments { get; set; }
 }
