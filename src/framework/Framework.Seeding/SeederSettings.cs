df537e081ea8ffb24963e4aaa6b54fda5d7c020d
Phil Schneider <info@philschneider.de>
2023-07-06 09:46:02 +0200

    chore: adding validation for seeder settings (#126)
    * add validation on application startup for the seeder settings
    ---------
    Reviewed-By: Norbert Truchsess <norbert.truchsess@t-online.de>

--- src/framework/Framework.Seeding/SeederSettings.cs
+++ src/framework/Framework.Seeding/SeederSettings.cs
@@ -1,3 +1,26 @@
+/********************************************************************************
+ * Copyright (c) 2021, 2023 BMW Group AG
+ * Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Org.Eclipse.TractusX.Portal.Backend.Framework.Models.Validation;
 using System.ComponentModel.DataAnnotations;
 
 namespace Org.Eclipse.TractusX.Portal.Backend.Framework.Seeding;
@@ -21,4 +44,20 @@ public class SeederSettings
     /// </summary>
     [Required]
     public IEnumerable<string> TestDataEnvironments { get; set; }
+}
+
+public static class SeederSettingsExtensions
+{
+    public static IServiceCollection ConfigureSeederSettings(
+        this IServiceCollection services,
+        IConfigurationSection section)
+    {
+        services.AddOptions<SeederSettings>()
+            .Bind(section)
+            .ValidateDataAnnotations()
+            .ValidateEnumEnumeration(section)
+            .ValidateDistinctValues(section)
+            .ValidateOnStart();
+        return services;
+    }
 }
