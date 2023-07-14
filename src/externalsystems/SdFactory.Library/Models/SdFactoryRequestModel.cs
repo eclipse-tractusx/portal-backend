9479aee0e37506f65ca2cf9f985a13a48e9b9232
Phil Schneider <info@philschneider.de>
2023-07-14 09:18:54 +0200

    chore: change LegalPerson to LegalParticipant for SD call
    Refs: CPLP-2962

--- src/externalsystems/SdFactory.Library/Models/SdFactoryRequestModel.cs
+++ src/externalsystems/SdFactory.Library/Models/SdFactoryRequestModel.cs
@@ -57,8 +57,8 @@ public record ConnectorSdFactoryRequestModel(
 
 public enum SdFactoryRequestModelSdType
 {
-    [EnumMember(Value = "LegalPerson")]
-    LegalPerson,
+    [EnumMember(Value = "LegalParticipant")]
+    LegalParticipant,
 
     [EnumMember(Value = "ServiceOffering")]
     ServiceOffering
