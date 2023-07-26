92953374ab28863c3c5dd0bb83ec651a90792da9
qxz2mqe <md-majid.akhter@partner.bmw.in>
2023-07-26 10:56:50 +0530

    tests: add unit tests for ServiceAccountRepository
    Refs: CPLP-2830

--- tests/portalbackend/PortalBackend.DBAccess.Tests/ServiceAccountRespotitoryTests.cs
+++ tests/portalbackend/PortalBackend.DBAccess.Tests/ServiceAccountRespotitoryTests.cs
@@ -174,6 +174,34 @@ public class ServiceAccountRepositoryTests : IAssemblyFixture<TestDbFixture>
         result.Should().BeNull();
     }
 
+    [Fact]
+    public async Task GetOwnCompanyServiceAccountDetailedDataUntrackedAsync_WithValidProviderWithDifferentOwner_ReturnsExpectedResult()
+    {
+        // Arrange
+        var (sut, _) = await CreateSut().ConfigureAwait(false);
+        Guid companyServiceAccountId = new("93eecd4e-ca47-4dd2-85bf-775ea72eb000");
+        Guid companyId = new("41fd2ab8-71cd-4546-9bef-a388d91b2542");
+        // Act
+        var result = await sut.GetOwnCompanyServiceAccountDetailedDataUntrackedAsync(companyServiceAccountId, companyId).ConfigureAwait(false);
+
+        // Assert
+        result.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task GetOwnCompanyServiceAccountDetailedDataUntrackedAsync_WithInvalidCompanyId_ReturnsNull()
+    {
+        // Arrange
+        var (sut, _) = await CreateSut().ConfigureAwait(false);
+        Guid companyServiceAccountId = new("93eecd4e-ca47-4dd2-85bf-775ea72eb000");
+        Guid companyId = new("41fd2ab8-71cd-4546-9bef-a388d91b2544");
+        // Act
+        var result = await sut.GetOwnCompanyServiceAccountDetailedDataUntrackedAsync(companyServiceAccountId, companyId).ConfigureAwait(false);
+
+        // Assert
+        result.Should().BeNull();
+    }
+
     #endregion
 
     #region GetOwnCompanyServiceAccountDetailedDataUntrackedAsync
