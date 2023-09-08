8f74c5f2ceb144d64498e28d6002949b8a574a5d
Norbert Truchsess <norbert.truchsess@t-online.de>
2023-09-08 16:52:14 +0200

    chore: adjust down of migration

--- src/portalbackend/PortalBackend.Migrations/Migrations/20230908111650_CPLP-3165-AddN2NModels.cs
+++ src/portalbackend/PortalBackend.Migrations/Migrations/20230908111650_CPLP-3165-AddN2NModels.cs
@@ -289,9 +289,7 @@ namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migration
 
             migrationBuilder.Sql("UPDATE portal.identity_providers set identity_provider_category_id = 1 where identity_provider_type_id = 3;");
             migrationBuilder.Sql("DELETE from portal.agreement_assigned_company_roles where company_role_id = 5");
-            migrationBuilder.Sql("DELETE from portal.company_role_descriptions where company_role_id = 5");
-            migrationBuilder.Sql("DELETE from portal.company_role_assigned_role_collections where company_role_id = 5");
-            migrationBuilder.Sql("DELETE from portal.company_role_registration_data where company_role_id = 5");
+            migrationBuilder.Sql("DELETE from portal.company_assigned_roles where company_role_id = 5");
 
             migrationBuilder.DropForeignKey(
                 name: "fk_company_applications_companies_onboarding_service_provider_",
