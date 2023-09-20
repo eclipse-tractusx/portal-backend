63c2a9f301c13066acd9cea59493a1be8e0026a5
evegufy <evelyn.gurschler@bmw.de>
2023-09-20 16:43:13 +0200

    chore(e2e-testing): prepare for merge - fix obsolete model reference - set AssemblyName and RootNamespace - fix packagereference and namespace in a couple of files - add file header - update reportportal package - update dependencies

--- tests/endtoend/CreateAppScenario/CreateNewTestAppScenario.cs
+++ tests/endtoend/CreateAppScenario/CreateNewTestAppScenario.cs
@@ -1,7 +1,26 @@
+/********************************************************************************
+ * Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
 using Castle.Core.Internal;
-using EndToEnd.Tests;
 using FluentAssertions;
 using Org.Eclipse.TractusX.Portal.Backend.Apps.Service.ViewModels;
+using Org.Eclipse.TractusX.Portal.Backend.EndToEnd.Tests;
 using Org.Eclipse.TractusX.Portal.Backend.Offers.Library.Models;
 using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.DBAccess.Models;
 using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities.Enums;
@@ -96,7 +115,7 @@ public class CreateNewTestAppScenario : EndToEndTestBase
             {
                 yield return new object[] { t };
             }
-        }   
+        }
     }
 
     private static bool ValidateStorageOfAppDetailData(AppRequestModel newAppRequest,
--- tests/endtoend/ServiceAccountCUDScenarios/ServiceAccountCUDScenarios.cs
+++ tests/endtoend/ServiceAccountCUDScenarios/ServiceAccountCUDScenarios.cs
@@ -1,6 +1,25 @@
+/********************************************************************************
+ * Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
 using Castle.Core.Internal;
-using EndToEnd.Tests;
 using FluentAssertions;
+using Org.Eclipse.TractusX.Portal.Backend.EndToEnd.Tests;
 using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.DBAccess.Models;
 using System.IdentityModel.Tokens.Jwt;
 using Xunit;
@@ -19,13 +38,6 @@ public class ServiceAccountCUDScenarios : EndToEndTestBase
 
     public ServiceAccountCUDScenarios(ITestOutputHelper output) : base(output)
     {
-    }
-
-    [Fact(DisplayName = "Service Account Get Access Token")]
-    private async Task GetAccessToken() // in order to just get token once, ensure that method name is alphabetically before other tests cases
-    {
-        var result = await AdministrationEndpointHelper.GetOperatorToken();
-        result.Should().BeTrue("Could not get an access token for technical user.");
     }
 
     //Scenario - Create a new service account
