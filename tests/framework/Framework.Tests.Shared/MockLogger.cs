e70f60871396ad6f63cb7201b406d69c01487ca8
Phil Schneider <info@philschneider.de>
2023-11-03 11:08:56 +0100

    feat(nuget): version upgrade
    Refs: CPLP-3400

--- tests/framework/Framework.Tests.Shared/MockLogger.cs
+++ tests/framework/Framework.Tests.Shared/MockLogger.cs
@@ -1,5 +1,4 @@
 /********************************************************************************
- * Copyright (c) 2021, 2023 BMW Group AG
  * Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
@@ -20,7 +19,7 @@
 
 using Microsoft.Extensions.Logging;
 
-namespace Org.Eclipse.TractusX.Portal.Backend.Tests.Shared;
+namespace Org.Eclipse.TractusX.Portal.Backend.Framework.Tests.Shared;
 
 public interface IMockLogger<T>
 {
