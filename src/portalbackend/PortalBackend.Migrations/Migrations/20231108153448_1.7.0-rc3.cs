a7f926cb2b0901448a7bb5e3c1cf1d5f76aefe78
Norbert Truchsess <norbert.truchsess@t-online.de>
2023-11-08 16:45:23 +0100

    fix(migrations): set shared-idps ownerId to linked company

---
+++ src/portalbackend/PortalBackend.Migrations/Migrations/20231108153448_1.7.0-rc3.cs
@@ -0,0 +1,54 @@
+/********************************************************************************
+ * Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class _170rc3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // set the owner of shared idps to the using company (if not ambigious)
+            migrationBuilder.Sql(@"UPDATE portal.identity_providers AS ip
+                SET owner_id = 
+                    (
+                        SELECT cip.company_id
+                        FROM portal.company_identity_providers AS cip
+                        WHERE cip.identity_provider_id = ip.id
+                    )
+                WHERE ip.identity_provider_type_id = 3
+                AND (
+                        SELECT COUNT(*)
+                        FROM portal.company_identity_providers
+                        WHERE identity_provider_id = ip.id
+                    ) = 1;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
