97e42ce12b528c3c629f71a976f0d19ea3f51823
AnuragNagpure <anurag.damodarnagpure@partner.bmw.in>
2023-12-14 11:36:18 +0530

    company name pattern implementation for post/put

--- tests/administration/Administration.Service.Tests/BusinessLogic/InvitationBusinessLogicTests.cs
+++ tests/administration/Administration.Service.Tests/BusinessLogic/InvitationBusinessLogicTests.cs
@@ -214,6 +214,7 @@ public class InvitationBusinessLogicTests
         A.CallTo(() => _mailingService.SendMails(A<string>._, A<Dictionary<string, string>>._, A<List<string>>._)).MustNotHaveHappened();
     }
 
+
     [Fact]
     public async Task TestExecuteInvitationCreateUserErrorThrows()
     {
