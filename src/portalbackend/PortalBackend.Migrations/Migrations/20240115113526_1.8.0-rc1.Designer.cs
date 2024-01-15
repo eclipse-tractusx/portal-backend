b8b29614c8772aed5a4d2a7b05904a24728ee96f
Phil Schneider <info@philschneider.de>
2024-01-15 12:48:25 +0100

    feat(migrations): merge all migrations into one
    merge all migrations that were created since v1.7.0-RC4 into one
    
    Refs: CPLP-3367

--- src/portalbackend/PortalBackend.Migrations/Migrations/20240115113526_1.8.0-rc1.Designer.cs
+++ src/portalbackend/PortalBackend.Migrations/Migrations/20240115113526_1.8.0-rc1.Designer.cs
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2021,2022 Contributors to the Eclipse Foundation
+ * Copyright (c) 2021, 2024 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -31,8 +31,8 @@ using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities;
 namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migrations
 {
     [DbContext(typeof(PortalDbContext))]
-    [Migration("20240102115525_CPLP-3469-AddAgreementView")]
-    partial class CPLP3469AddAgreementView
+    [Migration("20240115113526_1.8.0-rc1")]
+    partial class _180rc1
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
