fb239382c7cbfd9df99a1015d974d4f58ce37d6f
AnuragNagpure <anurag.damodarnagpure@partner.bmw.in>
2024-02-01 19:59:34 +0530

    Initial methods for post company certificate endpoint added

--- src/administration/Administration.Service/BusinessLogic/CompanyDataBusinessLogic.cs
+++ src/administration/Administration.Service/BusinessLogic/CompanyDataBusinessLogic.cs
@@ -364,7 +364,39 @@ public class CompanyDataBusinessLogic : ICompanyDataBusinessLogic
 
         await _portalRepositories.SaveAsync().ConfigureAwait(false);
     }
+    /// <inheritdoc />
+    // public async Task CreateCompanyCertificate(CertificateCreationData data, CancellationToken cancellationToken)
+    // {
 
+    //     var documentContentType = data.Document.ContentType.ParseMediaTypeId();
+    //     documentContentType.CheckDocumentContentType(_settings.SsiCertificateMediaTypes);
+
+    //     var companyCertificateRepository = _portalRepositories.GetInstance<ICompanyCertificateRepository>();
+    //     if (!await companyCertificateRepository.CheckSsiCertificateType(data.CertificateType.Id).ConfigureAwait(false))
+    //     {
+    //         throw new ControllerArgumentException($"{data.CertificateType.Id} is not assigned to a certificate");
+    //     }
+    //     await HandleCompanyCertificateCreationAsync(data.CertificateType.Id, null, data.Document, documentContentType, companyCertificateRepository, data.ExpiryDate, cancellationToken).ConfigureAwait(false);
+    // }
+    // /// <inheritdoc />
+    // private async Task HandleCompanyCertificateCreationAsync(CompanyCertificateTypeId companyCertificateTypeId, Guid? verifiedCompanyCertificateId, IFormFile document, MediaTypeId mediaTypeId,
+    // ICompanyCertificateRepository companyCertificateRepository, DateTimeOffset? expiryDate, CancellationToken cancellationToken)
+    // {
+    //     var (documentContent, hash) = await document.GetContentAndHash(cancellationToken).ConfigureAwait(false);
+    //     var doc = _portalRepositories.GetInstance<IDocumentRepository>().CreateDocument(document.FileName, documentContent,
+    //         hash, mediaTypeId, DocumentTypeId.Company_Certificate, x =>
+    //         {
+    //             x.CompanyUserId = _identityData.IdentityId;
+    //             x.DocumentStatusId = DocumentStatusId.PENDING;
+    //         });
+
+    //     // companyCertificateRepository.CreateCompanyCertificate(_identityData.CompanyId, companyCertificateTypeId, doc.Id, expiryDate ,n=>
+    //     // {
+           
+    //     // });
+
+    //     await _portalRepositories.SaveAsync().ConfigureAwait(false);
+    // }
     /// <inheritdoc />
     public async Task CreateCompanyCertificate(CompanyCertificateCreationData data, CancellationToken cancellationToken)
     {
