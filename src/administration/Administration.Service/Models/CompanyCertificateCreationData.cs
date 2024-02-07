88f1079fd3b32d7aab2401a6ef2e32c0165b7a94
AnuragNagpure <anurag.damodarnagpure@partner.bmw.in>
2024-02-08 00:57:43 +0530

    check formatting issues resolved and committed

--- src/administration/Administration.Service/Models/CompanyCertificateCreationData.cs
+++ src/administration/Administration.Service/Models/CompanyCertificateCreationData.cs
@@ -21,7 +21,6 @@ using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities.Enums;
 
 namespace Org.Eclipse.TractusX.Portal.Backend.Administration.Service.Models;
 
-
 public record CompanyCertificateCreationData
 (
     CompanyCertificateTypeId CertificateType,
