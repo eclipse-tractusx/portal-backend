c4ac0a995a643b08128506be89411579c7eaa3d3
AnuragNagpure <anurag.damodarnagpure@partner.bmw.in>
2024-02-13 18:19:13 +0530

    Updated migration for company certificate status added

---
+++ src/portalbackend/PortalBackend.Migrations/Migrations/20240213124258_CPLP-468-certificate-update.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class CPLP468certificateupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "portal",
+                table: "company_certificate_statuses",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "company_certificate_statuses",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "label",
+                value: "ACTIVE");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "company_certificate_statuses",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "label",
+                value: "INACTVIE");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "company_certificate_statuses",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "label",
+                value: "IN_REVIEW");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "company_certificate_statuses",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "label",
+                value: "ACTIVE");
+
+            migrationBuilder.InsertData(
+                schema: "portal",
+                table: "company_certificate_statuses",
+                columns: new[] { "id", "label" },
+                values: new object[] { 3, "INACTVIE" });
+        }
+    }
+}
