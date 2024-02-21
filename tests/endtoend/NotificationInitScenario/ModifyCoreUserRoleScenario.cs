717207992fc91dd4cb09ad5f3b7a9fbd00edc3ac
Norbert Truchsess <norbert.truchsess@t-online.de>
2024-02-21 12:25:33 +0100

    adjust end2end test

--- tests/endtoend/NotificationInitScenario/ModifyCoreUserRoleScenario.cs
+++ tests/endtoend/NotificationInitScenario/ModifyCoreUserRoleScenario.cs
@@ -191,6 +191,14 @@ public class ModifyCoreUserRoleScenario : EndToEndTestBase
         {
             throw new Exception($"Could not get company user details from {endpoint} should not be null.");
         }
+        if (companyUserDetails.FirstName is null)
+        {
+            throw new Exception($"Company user details from {endpoint} should not return firstName null");
+        }
+        if (companyUserDetails.LastName is null)
+        {
+            throw new Exception($"Company user details from {endpoint} should not return lastName null");
+        }
         _username = companyUserDetails.FirstName + " " + companyUserDetails.LastName;
         return companyUserDetails.CompanyUserId.ToString();
     }
