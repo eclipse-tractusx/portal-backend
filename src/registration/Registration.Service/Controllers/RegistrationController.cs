a53ddaaff4703059bb4d832a9ed2ddc07921fd37
Phil Schneider <info@philschneider.de>
2024-05-27 15:14:01 +0200

    fix(policy): add valid company policy (#775)
    Reviewed-By: Evelyn Gurschler <evelyn.gurschler@bmw.de>

--- src/registration/Registration.Service/Controllers/RegistrationController.cs
+++ src/registration/Registration.Service/Controllers/RegistrationController.cs
@@ -504,6 +504,7 @@ namespace Org.Eclipse.TractusX.Portal.Backend.Registration.Service.Controllers
         [HttpPost]
         [Authorize(Roles = "view_registration")]
         [Authorize(Policy = PolicyTypes.CompanyUser)]
+        [Authorize(Policy = PolicyTypes.ValidCompany)]
         [Route("applications/{applicationId}/declineregistration")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
