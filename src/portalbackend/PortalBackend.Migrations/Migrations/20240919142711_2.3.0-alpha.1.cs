6fcb310c2eb09bc82ff4fa06ee3b9c612115177d
Phil Schneider <info@philschneider.de>
2024-09-19 16:34:38 +0200

    build(migrations): aggregate migrations
    Refs: #1008

--- src/portalbackend/PortalBackend.Migrations/Migrations/20240919142711_2.3.0-alpha.1.cs
+++ src/portalbackend/PortalBackend.Migrations/Migrations/20240919142711_2.3.0-alpha.1.cs
@@ -18,13 +18,16 @@
  ********************************************************************************/
 
 using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 #nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
 namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migrations
 {
     /// <inheritdoc />
-    public partial class _811offerprovider : Migration
+    public partial class _230alpha1 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -38,15 +41,47 @@ namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migration
                 schema: "portal",
                 table: "offers");
 
+            migrationBuilder.Sql("DELETE from portal.process_steps WHERE process_step_type_id = 15");
+
+            migrationBuilder.DeleteData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 15);
+
+            migrationBuilder.Sql("DELETE from portal.process_steps WHERE process_step_type_id = 503");
+
+            migrationBuilder.DeleteData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 503);
+
             migrationBuilder.DropColumn(
                 name: "provider",
                 schema: "portal",
                 table: "offers");
 
+            migrationBuilder.DropColumn(
+                name: "organisation_name",
+                schema: "portal",
+                table: "company_invitations");
+
             migrationBuilder.AlterColumn<Guid>(
                 name: "provider_company_id",
                 schema: "portal",
                 table: "offers",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "application_id",
+                schema: "portal",
+                table: "company_invitations",
                 type: "uuid",
                 nullable: false,
                 defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
@@ -83,6 +118,104 @@ namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migration
                     table.PrimaryKey("pk_audit_offer20240911", x => x.audit_v1id);
                 });
 
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "label",
+                value: "MANUAL_VERIFY_REGISTRATION");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 9,
+                column: "label",
+                value: "AWAIT_CLEARING_HOUSE_RESPONSE");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 16,
+                column: "label",
+                value: "MANUAL_TRIGGER_OVERRIDE_CLEARING_HOUSE");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 18,
+                column: "label",
+                value: "AWAIT_SELF_DESCRIPTION_LP_RESPONSE");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 19,
+                column: "label",
+                value: "MANUAL_DECLINE_APPLICATION");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 21,
+                column: "label",
+                value: "AWAIT_DIM_RESPONSE_RESPONSE");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 26,
+                column: "label",
+                value: "AWAIT_BPN_CREDENTIAL_RESPONSE");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 28,
+                column: "label",
+                value: "AWAIT_MEMBERSHIP_CREDENTIAL_RESPONSE");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 101,
+                column: "label",
+                value: "AWAIT_START_AUTOSETUP");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 111,
+                column: "label",
+                value: "MANUAL_TRIGGER_ACTIVATE_SUBSCRIPTION");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 505,
+                column: "label",
+                value: "AWAIT_DELETE_DIM_TECHNICAL_USER_RESPONSE");
+
+            migrationBuilder.InsertData(
+                schema: "portal",
+                table: "process_step_types",
+                columns: new[] { "id", "label" },
+                values: new object[,]
+                {
+                    { 31, "RETRIGGER_REQUEST_BPN_CREDENTIAL" },
+                    { 32, "RETRIGGER_REQUEST_MEMBERSHIP_CREDENTIAL" }
+                });
+
             migrationBuilder.AddForeignKey(
                 name: "fk_offers_companies_provider_company_id",
                 schema: "portal",
@@ -114,6 +247,22 @@ namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migration
                 name: "audit_offer20240911",
                 schema: "portal");
 
+            migrationBuilder.Sql("DELETE from portal.process_steps WHERE process_step_type_id = 31");
+
+            migrationBuilder.DeleteData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 31);
+
+            migrationBuilder.Sql("DELETE from portal.process_steps WHERE process_step_type_id = 32");
+
+            migrationBuilder.DeleteData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 32);
+
             migrationBuilder.AlterColumn<Guid>(
                 name: "provider_company_id",
                 schema: "portal",
@@ -131,6 +280,121 @@ namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migration
                 maxLength: 255,
                 nullable: false,
                 defaultValue: "");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "application_id",
+                schema: "portal",
+                table: "company_invitations",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddColumn<string>(
+                name: "organisation_name",
+                schema: "portal",
+                table: "company_invitations",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "label",
+                value: "VERIFY_REGISTRATION");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 9,
+                column: "label",
+                value: "END_CLEARING_HOUSE");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 16,
+                column: "label",
+                value: "TRIGGER_OVERRIDE_CLEARING_HOUSE");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 18,
+                column: "label",
+                value: "FINISH_SELF_DESCRIPTION_LP");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 19,
+                column: "label",
+                value: "DECLINE_APPLICATION");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 21,
+                column: "label",
+                value: "AWAIT_DIM_RESPONSE");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 26,
+                column: "label",
+                value: "STORED_BPN_CREDENTIAL");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 28,
+                column: "label",
+                value: "STORED_MEMBERSHIP_CREDENTIAL");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 101,
+                column: "label",
+                value: "START_AUTOSETUP");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 111,
+                column: "label",
+                value: "TRIGGER_ACTIVATE_SUBSCRIPTION");
+
+            migrationBuilder.UpdateData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 505,
+                column: "label",
+                value: "AWAIT_DELETE_DIM_TECHNICAL_USER");
+
+            migrationBuilder.InsertData(
+                schema: "portal",
+                table: "process_step_types",
+                columns: new[] { "id", "label" },
+                values: new object[,]
+                {
+                    { 15, "OVERRIDE_BUSINESS_PARTNER_NUMBER" },
+                    { 503, "RETRIGGER_AWAIT_CREATE_DIM_TECHNICAL_USER_RESPONSE" }
+                });
 
             migrationBuilder.AddForeignKey(
                 name: "fk_offers_companies_provider_company_id",
