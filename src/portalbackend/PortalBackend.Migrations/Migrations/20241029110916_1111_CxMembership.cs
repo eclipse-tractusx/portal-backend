73716906f201a02d41888c66151be4e48ee68088
Tayyab Fayyaz Janjua <tayyab-fayyaz.janjua@cofinity-x.com>
2024-10-29 12:10:02 +0100

    added migration files

---
+++ src/portalbackend/PortalBackend.Migrations/Migrations/20241029110916_1111_CxMembership.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1111_CxMembership : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "portal",
+                table: "process_step_types",
+                columns: new[] { "id", "label" },
+                values: new object[,]
+                {
+                    { 44, "SET_CX_MEMBERSHIP_IN_BPDM" },
+                    { 45, "RETRIGGER_SET_CX_MEMBERSHIP_IN_BPDM" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 44);
+
+            migrationBuilder.DeleteData(
+                schema: "portal",
+                table: "process_step_types",
+                keyColumn: "id",
+                keyValue: 45);
+        }
+    }
+}
