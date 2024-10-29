5aa4b363ec4814f95a67954571d47fc935b0c2b9
Tayyab Fayyaz Janjua <tayyab-fayyaz.janjua@cofinity-x.com>
2024-10-29 12:10:02 +0100

    added migration files

--- src/portalbackend/PortalBackend.Migrations/Migrations/20241029110916_1111_CxMembership.Designer.cs
+++ src/portalbackend/PortalBackend.Migrations/Migrations/20241029110916_1111_CxMembership.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities;
@@ -12,9 +13,11 @@ using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities;
 namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migrations
 {
     [DbContext(typeof(PortalDbContext))]
-    partial class PortalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241029110916_1111_CxMembership")]
+    partial class _1111_CxMembership
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
