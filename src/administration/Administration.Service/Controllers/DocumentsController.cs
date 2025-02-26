4d6f61cfae483dbd59a7081c47cf7079fd38f0fd
Tayyab Fayyaz Janjua <tayyab-fayyaz.janjua@cofinity-x.com>
2025-02-26 12:54:01 +0100

    feat(sdFactory): selfdescription document anonymous allowed (#1297)
    * Update src/administration/Administration.Service/Controllers/DocumentsController.cs
    * Fix open information controller test for documents/selfdescription endpoint
    ---------
    Co-authored-by: Karsten Thiems <karsten.thiems@cofinity-x.com>
    Co-authored-by: Norbert Truchsess <norbert.truchsess@t-online.de>

--- src/administration/Administration.Service/Controllers/DocumentsController.cs
+++ src/administration/Administration.Service/Controllers/DocumentsController.cs
@@ -1,5 +1,4 @@
 /********************************************************************************
- * Copyright (c) 2022 BMW Group AG
  * Copyright (c) 2022 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
@@ -75,7 +74,7 @@ public class DocumentsController : ControllerBase
     }
 
     /// <summary>
-    /// Retrieves a specific document for the given id.
+    /// Retrieves a specific self description document for the given id.
     /// </summary>
     /// <param name="documentId" example="4ad087bb-80a1-49d3-9ba9-da0b175cd4e3">Id of the document to get.</param>
     /// <returns>Returns the file.</returns>
@@ -84,7 +83,7 @@ public class DocumentsController : ControllerBase
     /// <response code="404">The document was not found.</response>
     [HttpGet]
     [Route("selfDescription/{documentId}")]
-    [Authorize(Roles = "view_documents")]
+    [AllowAnonymous]
     [ProducesResponseType(typeof(FileContentResult), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
     [PublicUrl(CompanyRoleId.SERVICE_PROVIDER, CompanyRoleId.APP_PROVIDER)]
