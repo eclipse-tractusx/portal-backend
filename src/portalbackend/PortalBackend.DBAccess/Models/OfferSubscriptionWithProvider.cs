a255826c20e2b9e062b4ee2a6ccd87172f73211f
Tayyab Fayyaz Janjua <tayyab-fayyaz.janjua@cofinity-x.com>
2025-05-12 11:59:24 +0200

    fix(connector): sd-toggle check while updating connector url - Consistency of create, update and delete connector while getting provider company's country code. - Get provider company's country code to get country-specific settings. - Get country-specific settings to get correct sd-toggle to call sd-factory API.
    Ref: #1346

--- src/portalbackend/PortalBackend.DBAccess/Models/OfferSubscriptionWithProvider.cs
+++ src/portalbackend/PortalBackend.DBAccess/Models/OfferSubscriptionWithProvider.cs
@@ -1,6 +1,6 @@
 /********************************************************************************
- * Copyright (c) 2022 BMW Group AG
- * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ * Copyright (c) 2023 BMW Group AG
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -23,14 +23,23 @@ using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities.Enums;
 namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.DBAccess.Models;
 
 /// <summary>
-/// View model for connectors.
+/// Get offer subscription with offer provider
 /// </summary>
-public record ConnectorUpdateInformation(
-    ConnectorStatusId Status,
-    ConnectorTypeId Type,
-    bool IsProviderCompany,
-    string ConnectorUrl,
-    string? Bpn,
-    string? CountryAlpha2Code,
+/// <param name="Exists">Existence of offer subscription</param>
+/// <param name="IsOfferProvider">true if Company Id is the same as ProviderCompanyId</param>
+/// <param name="OfferSubscriptionAlreadyLinked">true if OfferSubscription is already linked to a connector</param>
+/// <param name="OfferSubscriptionStatus">Offer subscription status</param>
+/// <param name="SelfDescriptionDocumentId">Provider company sd document id</param>
+/// <param name="CompanyId">Host/Customer company id</param>
+/// <param name="ProviderBpn">Provider's Bpn</param>
+/// <param name="CountryAlpha2Code">Provider's country code</param>
+public record OfferSubscriptionWithProvider(
+    bool Exists,
+    bool IsOfferProvider,
+    bool OfferSubscriptionAlreadyLinked,
+    OfferSubscriptionStatusId OfferSubscriptionStatus,
     Guid? SelfDescriptionDocumentId,
-    Guid? SelfDescriptionCompanyDocumentId);
+    Guid CompanyId,
+    string? ProviderBpn,
+    string? CountryAlpha2Code
+);
